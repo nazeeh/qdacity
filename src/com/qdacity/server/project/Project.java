9a8fbaae0754042a120ebb5d9790dd41c15211f0
Andreas <andreas.kaufmann@fau.de>
2015-11-09 13:31:43 +0100

    Added name attribute to projects
    Projects now have a name attribute, that is set when the project is created.
    
    Fixes #93

--- src/com/qdacity/server/project/Project.java
+++ src/com/qdacity/server/project/Project.java
@@ -15,26 +15,44 @@ public class Project {
 	@PrimaryKey
 	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
 	Long id;
+	
+	@Persistent
+	String name;
+	
 	@Persistent
 	Long codesystemID;
+	
 	@Persistent
 	Long maxCodingID;
 	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	public Long getCodesystemID() {
 		return codesystemID;
 	}
+	
 	public void setCodesystemID(Long codesystemID) {
 		this.codesystemID = codesystemID;
 	}
+	
 	public Long getMaxCodingID() {
 		return maxCodingID;
 	}
+	
 	public void setMaxCodingID(Long maxCodingID) {
 		this.maxCodingID = maxCodingID;
 	}
