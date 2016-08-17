e1c63d726548a77cbc0aa584dcf08470464af35f
Andreas <andreas.kaufmann@fau.de>
2016-08-17 10:39:50 +0200

    Added ProjectType.java
    Overlooked for previous commit.
    
    Refs #198

---
+++ src/com/qdacity/project/ProjectType.java
@@ -0,0 +1,14 @@
+package com.qdacity.project;
+
+public enum ProjectType {
+  PROJECT(1), REVISION(2), VALIDATION_PROJECT(3);
+  private final int value;
+  private ProjectType(int value) {
+      this.value = value;
+  }
+
+  public int getValue() {
+      return value;
+  }
+}
+
