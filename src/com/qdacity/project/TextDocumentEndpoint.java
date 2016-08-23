ab2d57bea0293a7d7b7d7a311eaef58f9412d122
Andreas <andreas.kaufmann@fau.de>
2016-08-23 13:46:20 +0200

    Fixed getTextDocuments for ValidationProjects
    the text documents were not fetched for ValidationProjects. Was an issue of lower vs upper case parameter.
    
    Fixes #214

--- src/com/qdacity/project/TextDocumentEndpoint.java
+++ src/com/qdacity/project/TextDocumentEndpoint.java
@@ -109,7 +109,7 @@ public class TextDocumentEndpoint {
 	    if (prjType == "REVISION"){
 	      ProjectRevision validationProject = mgr.getObjectById(ProjectRevision.class, id);
 	      Authorization.checkAuthorization(validationProject.getProjectID(), user);
-	    } else if (prjType != null && prjType.equals("validation")){
+	    } else if (prjType != null && prjType.equals("VALIDATION")){
 	      //FIXME auth
 	    } else{
 	      Authorization.checkAuthorization(id, user);
