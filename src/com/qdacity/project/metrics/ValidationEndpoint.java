6993025bc5357cb27e1eb8da1aa8a3554d54b9fa
Andreas <andreas.kaufmann@fau.de>
2016-12-14 15:10:45 +0100

    Fixed encoding of email recipient
    Also moved email notification to its own task because if many documents were present the request timed out.
    
    Fixes #303

--- src/com/qdacity/project/metrics/ValidationEndpoint.java
+++ src/com/qdacity/project/metrics/ValidationEndpoint.java
@@ -1,6 +1,7 @@
 package com.qdacity.project.metrics;
 
 import java.text.DateFormat;
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -14,6 +15,7 @@ import java.util.logging.Logger;
 import javax.inject.Named;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
+import javax.jdo.Transaction;
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
@@ -29,6 +31,7 @@ import com.qdacity.project.Project;
 import com.qdacity.project.ValidationProject;
 import com.qdacity.project.data.TextDocument;
 import com.qdacity.project.data.TextDocumentEndpoint;
+import com.qdacity.project.metrics.tasks.DeferredEmailNotification;
 import com.qdacity.project.metrics.tasks.DeferredEvaluation;
 import com.qdacity.project.metrics.tasks.DeferredReportDeletion;
 import com.qdacity.Sendgrid;
@@ -184,95 +187,12 @@ public class ValidationEndpoint {
       clientIds = {Constants.WEB_CLIENT_ID, 
          com.google.api.server.spi.Constant.API_EXPLORER_CLIENT_ID},
          audiences = {Constants.WEB_CLIENT_ID})
-  public void sendNotificationEmail(@Named("reportID") Long reportID, User user) throws UnauthorizedException, JSONException {
+  public void sendNotificationEmail(@Named("reportID") Long reportID, User user) throws UnauthorizedException {
     
-    PersistenceManager mgr = getPersistenceManager();
-    
-    
-    try {
-      ValidationReport report = mgr.getObjectById(ValidationReport.class, reportID);
-      // FIXME
-      List<ValidationResult> results;
-      
-      Query q2 = mgr.newQuery(ValidationResult.class, "reportID  == :reportID");
-//    Map<String, Long> params = new HashMap();
-//    params.put("reportID", repID);
-    results = (List<ValidationResult>) q2.execute(reportID);
-
-
-      
-//      List<ValidationResult> results = report.getValidationResult();
-      
-      for (ValidationResult result : results) {
-        Sendgrid mail = new Sendgrid(Credentials.SENDGRID_USER ,Credentials.SENDGRID_PW);
-        Long prjID = result.getValidationProjectID();
-        ValidationProject project =  mgr.getObjectById(ValidationProject.class, prjID);
-        List<String> coderIDs = project.getValidationCoders();
-        String greetingName = "";
-        for (String coderID : coderIDs) {
-          com.qdacity.user.User coder =  mgr.getObjectById(com.qdacity.user.User.class, coderID);
-          Logger.getLogger("logger").log(Level.INFO,   "Sending notification mail to: " + coder.getGivenName() + " " +coder.getSurName());
-          mail.addTo(coder.getEmail(), coder.getGivenName() + " " +coder.getSurName());
-          greetingName += coder.getGivenName() +", ";
-        }
-        mail.addTo("kaufmann@group.riehle.org", "Andreas Kaufmann");
-        String msgBody = "Hi "+greetingName+"<br>";
-        msgBody += "<p>";
-        msgBody +="We have analyzed your codings, and you've achieved the following scores:<br>";
-        msgBody += "</p>";
-        msgBody += "<p>";
-        msgBody +="<strong>Overall</strong> <br>";
-        msgBody +="F-Measure: "+result.getParagraphAgreement().getFMeasure()+"<br>";
-        msgBody +="Recall: "+result.getParagraphAgreement().getRecall()+"<br>";
-        msgBody +="Precision: "+result.getParagraphAgreement().getPrecision()+"<br>";
-        msgBody += "</p>";
-        msgBody += "<p>";
-        msgBody +="<strong>Document specific data:</strong><br>";
-        msgBody += "</p>";
-        
-        
-        List<DocumentResult> docResults = listDocumentResults(result.getId(), user);
-        for (DocumentResult documentResult : docResults) {
-          msgBody += "<p>";
-          msgBody +="<strong>"+documentResult.getDocumentName()+":</strong><br>";
-          msgBody +="F-Measure: "+documentResult.getParagraphAgreement().getFMeasure()+"<br>";
-          msgBody +="Recall: "+documentResult.getParagraphAgreement().getRecall()+"<br>";
-          msgBody +="Precision: "+documentResult.getParagraphAgreement().getPrecision()+"<br><br>";
-          msgBody += "</p>";
-        }
-        
-        msgBody += "<p>";
-        msgBody +="You may compare these values to the average of this report<br>";
-        msgBody +="F-Measure: "+report.getParagraphAgreement().getFMeasure()+"<br>";
-        msgBody +="Recall: "+report.getParagraphAgreement().getRecall()+"<br>";
-        msgBody +="Precision: "+report.getParagraphAgreement().getPrecision()+"<br><br>";
-        msgBody += "</p>";
-        
-        msgBody += "<p>";
-        msgBody +="This email is generated for this report: "+report.getName()+"<br>";
-        String reportTime = DateFormat.getDateInstance().format(report.getDatetime());
-        
-        msgBody +="Date of this report: "+reportTime+"<br>";
-        
-        msgBody += "<p>";
-        msgBody +="Best regards,<br>";
-        msgBody +="QDAcity Mailbot<br>";
-        msgBody += "</p>";
-        
-        mail.setFrom("QDAcity <support@qdacity.com>");
-        mail.setSubject("QDAcity Report");
-        mail.setText(" ").setHtml(msgBody);
-        
-        mail.send();
-        
-        Logger.getLogger("logger").log(Level.INFO,   mail.getServerResponse());
-        
-      }
-    } finally {
-      mgr.close();
-    }
-   
-     
+    DeferredEmailNotification task = new DeferredEmailNotification(reportID, user);
+    // Set instance variables etc as you wish
+   Queue queue = QueueFactory.getDefaultQueue();
+   queue.add(com.google.appengine.api.taskqueue.TaskOptions.Builder.withPayload(task));
   }
   
   @ApiMethod(name = "validation.deleteReport",   scopes = {Constants.EMAIL_SCOPE},
@@ -282,6 +202,8 @@ public class ValidationEndpoint {
   public List<ValidationReport> deleteReport(@Named("reportID") Long repID, User user) throws UnauthorizedException {
     List<ValidationReport> reports = new ArrayList<ValidationReport>(); //FIXME Why?
     PersistenceManager mgr = getPersistenceManager();
+    Transaction tx = mgr.currentTransaction();
+    
     try {
       ValidationReport report = mgr.getObjectById(ValidationReport.class, repID);
       
@@ -301,7 +223,7 @@ public class ValidationEndpoint {
       }
       
       //Lazy fetch
-      report.getParagraphAgreement();
+      //report.getParagraphAgreement();
       List<DocumentResult> docResults = report.getDocumentResults();
       for (DocumentResult documentResult : docResults) {
         documentResult.getAgreementMap();
