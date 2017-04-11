f1d4bae461a214daa668e55ebf6298bf2a05fa5e
Matthias Schoepe <matthias.schoepe@fau.de>
2017-04-11 18:10:42 +0200

    Introducing Template as Supertype for Algrothm Tasks. Started to Refactor DeferredFMeasureEvaluation accordingly.

---
+++ src/com/qdacity/project/metrics/tasks/algorithms/DeferredAlgorithmEvaluation.java
@@ -0,0 +1,55 @@
+package com.qdacity.project.metrics.tasks.algorithms;
+
+import com.google.api.server.spi.response.UnauthorizedException;
+import com.google.appengine.api.taskqueue.DeferredTask;
+import com.google.appengine.api.users.User;
+import com.qdacity.PMF;
+import com.qdacity.project.ValidationProject;
+import com.qdacity.project.metrics.ValidationResult;
+import javax.jdo.PersistenceManager;
+
+/**
+ * Template class for DeferredTasks running an EvaluationAlgorithm which
+ * produces a ValidationResult
+ */
+public abstract class DeferredAlgorithmEvaluation implements DeferredTask {
+
+    protected PersistenceManager mgr;
+    protected ValidationResult valResult;
+    protected final ValidationProject validationProject;
+    protected final User user;
+    protected final Long validationReportId;
+
+    public DeferredAlgorithmEvaluation(ValidationProject validationProject, User user, Long validationReportId) {
+	this.validationProject = validationProject;
+	this.user = user;
+	this.validationReportId = validationReportId;
+    }
+
+    @Override
+    public void run() {
+	try {
+	    mgr = getPersistenceManager();
+	    valResult = new ValidationResult();
+	    mgr.makePersistent(valResult); // make persistent to generate ID which is passed to deferred persistence of DocumentResults
+	    runAlgorithm();
+	    valResult.setName(validationProject.getCreatorName());
+	    valResult.setRevisionID(validationProject.getRevisionID());
+	    valResult.setValidationProjectID(validationProject.getId());
+	    valResult.setReportID(validationReportId);
+	    mgr.setMultithreaded(true); // FIXME needed?
+	    mgr.makePersistent(valResult);
+	} catch (Exception e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} finally {
+	    mgr.close();
+	}
+    }
+
+    protected abstract void runAlgorithm() throws Exception;
+
+    protected static PersistenceManager getPersistenceManager() {
+	return PMF.get().getPersistenceManager();
+    }
+}
