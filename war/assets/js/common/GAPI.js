226acd08cb79ef10cf50d9a48cd1b87fb9f8e57c
Felix Loos <loos.felix93@gmail.com>
2017-04-24 11:22:00 +0200

    -- Preparation for merge request

--- war/assets/js/common/GAPI.js
+++ war/assets/js/common/GAPI.js
@@ -1,9 +1,9 @@
 import Account from './Account.jsx';
 
-var scopes = 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
-var client_id = '62776117166-iuu5qlid44hnas1ivvurlr0cku1f0hnc.apps.googleusercontent.com';
-var api_path = 'https://1-dot-felix-lo-qdacity.appspot.com/_ah/api'; //'https://localhost:8888/_ah/api'
-var api_version = 'v1';
+var scopes = 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'; 
+var client_id = '309419937441-6d41vclqvedjptnel95i2hs4hu75u4v7.apps.googleusercontent.com';
+var api_path = 'https://4-dot-qdacity-app.appspot.com/_ah/api'; //'https://localhost:8888/_ah/api'
+var api_version = 'v4';
 
 export default function loadGAPIs(allLoadedCallback) {
 	var _this = this;
@@ -19,7 +19,6 @@ export default function loadGAPIs(allLoadedCallback) {
 			}
 			apisToLoad = 2;
 			//Parameters are APIName,APIVersion,CallBack function,API Root
-			//gapi.client.load('qdacity', api_version, callback, api_path);
 			gapi.client.load('qdacity', api_version, callback, api_path);
 			gapi.load('auth2', callback);
 		}
