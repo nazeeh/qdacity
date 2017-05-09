6392c3407b16a81f1dbf47ce4c6ac0b96276b5df
Felix Loos <loos.felix93@gmail.com>
2017-05-09 13:46:03 +0200

    Implemented edge translation depending on code relationship

--- war/assets/js/pages/uml-editor/MyEditorView.jsx
+++ war/assets/js/pages/uml-editor/MyEditorView.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
 export default class MyEditorView extends React.Component {
-
+    
 	constructor(props) {
 		super(props);
 		
 		this.graph = null;
 		this.layout = null;
+		
+		this.edgeTypes = this.props.edgeTypes;
+		
+		this.edgeStyles = {}
+        this.edgeStyles[this.edgeTypes.NONE] = 'startArrow=none;endArrow=none;';
+		this.edgeStyles[this.edgeTypes.GENERALIZATION] = 'startArrow=none;endArrow=' + mxConstants.ARROW_BLOCK + ';endFill=0;';
+		this.edgeStyles[this.edgeTypes.DEPENDENCY] = 'startArrow=none;endArrow=' + mxConstants.ARROW_OPEN + ';dashed=1';
+		this.edgeStyles[this.edgeTypes.AGGREGATION] = 'startArrow=' + mxConstants.ARROW_DIAMOND_THIN + ';endArrow=none;startFill=0;startSize=20;';
+		this.edgeStyles[this.edgeTypes.CONTAINMENT] = 'startArrow=' + mxConstants.ARROW_DIAMOND_THIN + ';endArrow=none;startFill=1;startSize=20;';
+		this.edgeStyles[this.edgeTypes.ASSOCIATION] = 'startArrow=none;endArrow=none;';
+		this.edgeStyles[this.edgeTypes.DIRECTED_ASSOCIATION] = 'startArrow=none;endArrow=' + mxConstants.ARROW_OPEN + ';';
+		this.edgeStyles[this.edgeTypes.REALIZATION] = 'startArrow=none;endArrow=' + mxConstants.ARROW_BLOCK + ';endFill=0;dashed=1';
 	}
 
 	run() {
@@ -17,33 +29,122 @@ export default class MyEditorView extends React.Component {
 
 		// Creates the graph inside the given container
 		this.graph = new mxGraph(container);
+		
+		
+        // WHAT DOES THAT DO?
+        this.graph.setPanning(true);
+        //this.graph.alternateEdgeStyle = 'elbow=vertical';
+		
+        var styleX = this.graph.getStylesheet().getDefaultEdgeStyle();
+        styleX[mxConstants.STYLE_EDGE] = mxEdgeStyle.SegmentConnector;//mxEdgeStyle.OrthConnector;
+        //
+        
+        
+        // Sets default styles
+        var style = this.graph.getStylesheet().getDefaultVertexStyle();
+        style[mxConstants.STYLE_FILLCOLOR] = '#FFFFFF';
+        style[mxConstants.STYLE_STROKECOLOR] = '#000000';
+        style[mxConstants.STYLE_FONTCOLOR] = '#000000';
+        style[mxConstants.STYLE_FONTSTYLE] = '1';
+        
+        style = this.graph.getStylesheet().getDefaultEdgeStyle();
+        style[mxConstants.STYLE_STARTSIZE] = '13';
+        style[mxConstants.STYLE_ENDSIZE] = '13';
+        style[mxConstants.STYLE_STROKECOLOR] = '#000000';
+        style[mxConstants.STYLE_FONTCOLOR] = '#000000';
+        style[mxConstants.STYLE_FONTSTYLE] = '0';
+		
+		
+		
 		// Enables rubberband selection
 		new mxRubberband(this.graph);
 
 		
 		this.layout = new mxFastOrganicLayout(this.graph);
         // Moves stuff wider apart than usual
-        this.layout.forceConstant = 80;
-		
-		
-		
-		
-		// Gets the default parent for inserting new cells. This
-		// is normally the first child of the root (ie. layer 0).
-//		var parent = this.graph.getDefaultParent();
-
-		// Adds cells to the model in a single step
-//		this.graph.getModel().beginUpdate();
-//		try {
-//			var v1 = this.graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30);
-//			var v2 = this.graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
-//			var e1 = this.graph.insertEdge(parent, null, '', v1, v2);
-//		} finally {
-//			// Updates the display
-//			this.graph.getModel().endUpdate();
-//		}
+        this.layout.forceConstant = 180;
 	}
 
+	/*
+	addClass() {
+	    var field = new mxCell('+ field: type', new mxGeometry(0, 0, 100, 26), 'text;html=1;strokeColor=none;fillColor=none;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;whiteSpace=wrap;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;');
+	    field.vertex = true;
+	    
+	    var divider = new mxCell('', new mxGeometry(0, 0, 40, 8), 'line;html=1;strokeWidth=1;fillColor=none;align=left;verticalAlign=middle;spacingTop=-1;spacingLeft=3;spacingRight=3;rotatable=0;labelPosition=right;points=[];portConstraint=eastwest;');
+	    divider.vertex = true;
+	    
+	    var style = 'swimlane;html=1;fontStyle=1;align=center;verticalAlign=top;childLayout=stackLayout;horizontal=1;startSize=26;horizontalStack=0;resizeParent=1;resizeLast=0;collapsible=1;marginBottom=0;swimlaneFillColor=#ffffff;';
+	    var cell = new mxCell('Classname', new mxGeometry(0, 0, 160, 90), style);
+        cell.vertex = true;
+        cell.insert(field.clone());
+        cell.insert(divider.clone());
+        //cell.insert(sb.cloneCell(field, '+ method(type): type'));
+        
+        return createItem([cell], cell.geometry.width, cell.geometry.height, 'Class'); 
+	}
+	
+	
+	createItem(cells, title, showLabel, showTitle, width, height, allowCellsInserted) {
+	    var elt = document.createElement('a');
+	    elt.setAttribute('href', 'javascript:void(0);');
+	    elt.className = 'geItem';
+	    elt.style.overflow = 'hidden';
+	    var border = (mxClient.IS_QUIRKS) ? 8 + 2 * this.thumbPadding : 2 * this.thumbBorder;
+	    elt.style.width = (this.thumbWidth + border) + 'px';
+	    elt.style.height = (this.thumbHeight + border) + 'px';
+	    elt.style.padding = this.thumbPadding + 'px';
+	    
+	    if (mxClient.IS_IE6)
+	    {
+	        elt.style.border = 'none';
+	    }
+	    
+	    // Blocks default click action
+	    mxEvent.addListener(elt, 'click', function(evt)
+	    {
+	        mxEvent.consume(evt);
+	    });
+
+	    this.createThumb(cells, this.thumbWidth, this.thumbHeight, elt, title, showLabel, showTitle, width, height);
+	    var bounds = new mxRectangle(0, 0, width, height);
+	    
+	    if (cells.length > 1 || cells[0].vertex)
+	    {
+	        var ds = this.createDragSource(elt, this.createDropHandler(cells, true, allowCellsInserted,
+	            bounds), this.createDragPreview(width, height), cells, bounds);
+	        this.addClickHandler(elt, ds, cells);
+	    
+	        // Uses guides for vertices only if enabled in graph
+	        ds.isGuidesEnabled = mxUtils.bind(this, function()
+	        {
+	            return this.editorUi.editor.graph.graphHandler.guidesEnabled;
+	        });
+	    }
+	    else if (cells[0] != null && cells[0].edge)
+	    {
+	        var ds = this.createDragSource(elt, this.createDropHandler(cells, false, allowCellsInserted,
+	            bounds), this.createDragPreview(width, height), cells, bounds);
+	        this.addClickHandler(elt, ds, cells);
+	    }
+	    
+	    // Shows a tooltip with the rendered cell
+	    if (!mxClient.IS_IOS)
+	    {
+	        mxEvent.addGestureListeners(elt, null, mxUtils.bind(this, function(evt)
+	        {
+	            if (mxEvent.isMouseEvent(evt))
+	            {
+	                this.showTooltip(elt, cells, bounds.width, bounds.height, title, showLabel);
+	            }
+	        }));
+	    }
+	    
+	    return elt;
+	};
+	*/
+	
+	
+	
 	addNode(name) {
 		var parent = this.graph.getDefaultParent();
 
@@ -59,14 +160,14 @@ export default class MyEditorView extends React.Component {
 		return node;
 	}
 	
-	addEdge(v1, v2) {
+	addEdge(v1, v2, edgeType) {
         var parent = this.graph.getDefaultParent();
         
 	    this.graph.getModel().beginUpdate();
         
 	    var edge;
 	    try {
-            edge = this.graph.insertEdge(parent, null, '', v1, v2);
+            edge = this.graph.insertEdge(parent, null, '', v1, v2, this.edgeStyles[edgeType]);
         } finally {
             this.graph.getModel().endUpdate();
         }   
--- war/assets/js/pages/uml-editor/uml-editor.js
+++ war/assets/js/pages/uml-editor/uml-editor.js
@@ -3,8 +3,9 @@ import MyEditorView from './MyEditorView.jsx';
 import Account from '../../common/Account.jsx';
 import loadGAPIs from '../../common/GAPI';
 
-import ProjectEndpoint from '../../common/endpoints/ProjectEndpoint';
 import CodesystemEndpoint from '../../common/endpoints/CodesystemEndpoint';
+import MetaModelEntityEndpoint from '../../common/endpoints/MetaModelEntityEndpoint';
+import ProjectEndpoint from '../../common/endpoints/ProjectEndpoint';
 
 import 'script!../../../../components/URIjs/URI.min.js';
 
@@ -14,6 +15,17 @@ $script('https://apis.google.com/js/platform.js', function () {
 	$script('https://apis.google.com/js/client.js?onload=init', 'google-api');
 });
 
+
+var edgeTypes = {
+	NONE: 0,
+	GENERALIZATION: 1,
+	DEPENDENCY: 2,
+	AGREGATION: 3,
+	CONTAINMENT: 4,
+	ASSOCIATION: 5,
+	DIRECTED_ASSOCIATION: 6,
+	REALIZATION: 7
+};
 
 var project_id;
 var project_type;
@@ -37,7 +49,7 @@ window.init = function () {
 	}
 
 
-	view = ReactDOM.render(<MyEditorView projectId={project_id} />, document.getElementById('content'));
+	view = ReactDOM.render(<MyEditorView projectId={project_id} edgeTypes={edgeTypes} />, document.getElementById('content'));
 	view.run();
 
 
@@ -46,6 +58,26 @@ window.init = function () {
 			account = accountModule;
 		}
 	);
+
+//	var v1 = view.addNode("1");
+//	var v2 = view.addNode("2");
+//	var v3 = view.addNode("3");
+//	var v4 = view.addNode("4");
+//	var v5 = view.addNode("5");
+//	var v6 = view.addNode("6");
+//	var v7 = view.addNode("7");
+//	var v8 = view.addNode("8");
+//	
+//	view.addEdge(v1, v2, generialization);
+//	view.addEdge(v2, v3, dependency);
+//	view.addEdge(v3, v4, aggregation);
+//	view.addEdge(v4, v5, containment);
+//	view.addEdge(v5, v6, association);
+//	view.addEdge(v6, v7, directed_association);
+//	view.addEdge(v7, v8, realization);
+//    
+//    
+	
 }
 
 function setupUI() {
@@ -63,46 +95,79 @@ function setupUI() {
 }
 
 function loadCodes(codesystem_id) {
-	var codes = [];
-
 	CodesystemEndpoint.getCodeSystem(codesystem_id).then(function (resp) {
-		resp.items = resp.items || [];
-		codes = resp.items;
-
-		var nodes = new Map();
-		var relations = [];
+		var codes = resp.items || [];
 		
-		for (var i = 0; i < codes.length; i++) {
-						
-			var node = view.addNode(codes[i].name);
-			nodes.set(codes[i].codeID, { 'code': codes[i], 'node': node });
+		MetaModelEntityEndpoint.listEntities(1).then(function (resp) {
+			var mmEntities = resp.items || [];
 			
-			
-			console.log('add ' + codes[i].codeID + ' - ' + codes[i].name);
-			
-			if (codes[i].relations != null) {
-				for (var j = 0; j < codes[i].relations.length; j++) {
-					console.log(codes[i].codeID + ' is connected to ' + codes[i].relations[j].codeId);
+			initGraph(codes, mmEntities);
+		});
+	});	
+}
+
+function initGraph(codes, mmEntities) {
+	var nodes = new Map();
+	var relations = [];
+	
+	for (var i = 0; i < codes.length; i++) {
 					
-					relations.push({ 'start': codes[i].codeID, 'end': codes[i].relations[j].codeId });
-				}
+		var node = view.addNode(codes[i].name);
+		nodes.set(codes[i].codeID, { 'code': codes[i], 'node': node });
+		
+		
+		console.log('add ' + codes[i].codeID + ' - ' + codes[i].name);
+		
+		if (codes[i].relations != null) {
+			for (var j = 0; j < codes[i].relations.length; j++) {
+				console.log(codes[i].codeID + ' is connected to ' + codes[i].relations[j].codeId);
+				
+				relations.push({ 'start': codes[i].codeID, 'end': codes[i].relations[j].codeId, 'metaModelEntityId': codes[i].relations[j].mmElementId });
+			}
+		}
+	}
+	
+	
+	
+	
+	for (var i = 0; i < relations.length; i++) {
+		var relation = relations[i];
+		
+		var start = nodes.get(relation.start).node;
+		var end = nodes.get(relation.end).node;
+		
+		
+		
+		
+		
+		var metaModelEntity = mmEntities.find(function(mmEntity) { return mmEntity.id == relations[i].metaModelEntityId;});
+		
+		var mode = null;
+		
+		switch (metaModelEntity.name) {
+			case 'is a': {
+				mode = edgeTypes.GENERALIZATION;
+				break;
+			}
+			case 'is part of': {
+				mode = edgeTypes.AGGREGATION;
+				break;
+			}
+			case 'is related to': {
+				mode = edgeTypes.DIRECTED_ASSOCIATION;
+				break;
+			}
+			default: {
+				// ERROR?
+				//alert("error??");
+				mode = edgeTypes.ASSOCIATION;
+				break;
 			}
 		}
 		
-		
-		
-		
-		for (var i = 0; i < relations.length; i++) {
-			var relation = relations[i];
-			
-			var start = nodes.get(relation.start).node;
-			var end = nodes.get(relation.end).node;
-			
-			view.addEdge(start, end);
-		}
+		view.addEdge(start, end, mode);
+	}
 
-		
-		view.applyLayout();
-		
-	});	
+	
+	view.applyLayout();
 }
