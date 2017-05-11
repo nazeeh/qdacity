656d8e6d18be7132ce822a2de9e9291a48a599e2
Felix Loos <loos.felix93@gmail.com>
2017-05-12 00:38:16 +0200

    Improved MetaModelMapper test

--- war/tests/MetaModelMapperTest.js
+++ war/tests/MetaModelMapperTest.js
@@ -4,10 +4,16 @@ import MetaModelMapper from '../assets/js/pages/uml-editor/MetaModelMapper.js';
 describe("MetaModelMapper", function() {
 	
 	it('gets correct edge type', function() {
-		const metaModelEntity = { 'name': 'is a' };
+		const metaModel_generalization = { 'name': 'is a' };
+		const metaModel_aggregation = { 'name': 'is part of' };
+		const metaModel_directed_association = { 'name': 'is related to' };
 		
-		let result = MetaModelMapper.getEdgeType(metaModelEntity)
+		let expect_gerneralization = MetaModelMapper.getEdgeType(metaModel_generalization)
+		let expect_aggregation = MetaModelMapper.getEdgeType(metaModel_aggregation)
+		let expect_directed_association = MetaModelMapper.getEdgeType(metaModel_directed_association)
 		
-		expect(result).toEqual(EdgeType.GENERALIZATION);
+		expect(expect_gerneralization).toEqual(EdgeType.GENERALIZATION);
+		expect(expect_aggregation).toEqual(EdgeType.AGGREGATION);
+		expect(expect_directed_association).toEqual(EdgeType.DIRECTED_ASSOCIATION);
 	});
 });
