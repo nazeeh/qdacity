b55fb4ebcaeb1bdc29b3f22d62fda7ef50de331d
Matthias Schoepe <matthias.schoepe@fau.de>
2017-05-16 10:57:04 +0200

    DRAFT DataMigrationEndpoint - structure complete. Open questions and assumptions see comments in the code.

--- src/com/qdacity/endpoint/DataMigrationEndpoint.java
+++ src/com/qdacity/endpoint/DataMigrationEndpoint.java
@@ -42,46 +42,92 @@ public class DataMigrationEndpoint {
 	    audiences = {Constants.WEB_CLIENT_ID})
     public void migrateV4toV5(User user) {
 	//assumption: Data only contains ValidationReports that have run an FMeasure!
-	migrateValidationReports(user);
-	migrateDocumentResults(user);
-	migrateValidationResults(user);
+	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	migrateValidationReports(user, datastore);
+	migrateDocumentResults(user, datastore);
+	migrateValidationResults(user, datastore);
 	//Needs to be done last, as others used ParagraphAgreement
-	migrateParagraphAgreements(user);
+	migrateParagraphAgreements(user, datastore); //Gibt es überhaupt "freie" ParagraphAgreements? Oder sind schon alle abgefertigt mit den ersten drei Schritten?
     }
 
     private static PersistenceManager getPersistenceManager() {
 	return PMF.get().getPersistenceManager();
     }
 
-    private void migrateValidationReports(User user) {
+    private void migrateValidationReports(User user, DatastoreService datastore) {
 	TabularValidationReportRow standardFMeasureHeader = createStandardFMeasureHeader();
 	//Column: paragraphAgreement -> avgAgreement (String) value: "Average, <fmeasure>, <recall>, <precision>"
 	//New column: avgAgreementHead (String) value: "Coder,FMeasure,Recall,Precision"
 	//New column: detailedAgreementHead (String) value: "Coder,FMeasure,Recall,Precision"
 	//New column: evaluationUnit (String) value: "paragraph"
-	for (Entity result : getAll("ValidationReport")) {
-	    //TODO
+	Iterable<Entity> reports = getAll("ValidationReport", datastore);
+	for (Entity report : reports) {
+	    //HINT: Problem: Class ParagraphAgreement does not exist any more. We need to deal with it manually.
+	    Entity avgParagraphAgreementEntity = (Entity) report.getProperty("paragraphAgreement"); //TODO funktioniert das überhaupt so?
+	    String fmeasure = (String) avgParagraphAgreementEntity.getProperty("fmeasure");
+	    String recall = (String) avgParagraphAgreementEntity.getProperty("recall");
+	    String precision = (String) avgParagraphAgreementEntity.getProperty("precision");
+
+	    report.setProperty("reportRow", "Average," + fmeasure + "," + recall + "," + precision);
+	    report.setProperty("evaluationUnit", "paragraph");
+	    report.setProperty("avgAgreementHead", standardFMeasureHeader.toString());
+	    report.setProperty("detailedAgreementHead", standardFMeasureHeader.toString());
+	    report.removeProperty("paragraphAgreement");
+	    //TODO das ParagraphAgreement selbst muss man hier noch löschen! (oder passiert das automatisch?)
 	}
+	//Persist all changes at once
+	datastore.put(reports);
     }
 
-    private void migrateDocumentResults(User user) {
+    private void migrateDocumentResults(User user, DatastoreService datastore) {
 	//Column: paragraphAgreement -> reportRow (String) value: "<documentName>, <fmeasure>, <recall>, <precision>"
-	for (Entity result : getAll("DocumentResult")) {
-	    //TODO
+	Iterable<Entity> docResults = getAll("DocumentResult", datastore);
+	for (Entity result : docResults) {
+	    //HINT: Problem: Class ParagraphAgreement does not exist any more. We need to deal with it manually.
+	    Entity paragraphAgreementEntity = (Entity) result.getProperty("paragraphAgreement"); //TODO funktioniert das überhaupt so?
+	    String fmeasure = (String) paragraphAgreementEntity.getProperty("fmeasure");
+	    String recall = (String) paragraphAgreementEntity.getProperty("recall");
+	    String precision = (String) paragraphAgreementEntity.getProperty("precision");
+
+	    String coderName = (String) result.getProperty("name");
+
+	    result.setProperty("reportRow", coderName + "," + fmeasure + "," + recall + "," + precision);
+	    result.removeProperty("name");
+	    result.removeProperty("paragraphAgreement");
+	    //TODO das ParagraphAgreement selbst muss man hier noch löschen! (oder passiert das automatisch?)
 	}
+	//Persist all changes at once
+	datastore.put(docResults);
     }
 
-    private void migrateValidationResults(User user) {
+    private void migrateValidationResults(User user, DatastoreService datastore) {
 	//Column: paragraphAgreement -> reportRow (String) value: "<coderName>, <fmeasure>, <recall>, <precision>"
-	for (Entity result : getAll("ValidationResult")) {
-	    //TODO
+	Iterable<Entity> validationResults = getAll("ValidationResult", datastore);
+	for (Entity result : validationResults) {
+
+	    //HINT: Problem: Class ParagraphAgreement does not exist any more. We need to deal with it manually.
+	    Entity paragraphAgreementEntity = (Entity) result.getProperty("paragraphAgreement"); //TODO funktioniert das überhaupt so?
+	    String fmeasure = (String) paragraphAgreementEntity.getProperty("fmeasure");
+	    String recall = (String) paragraphAgreementEntity.getProperty("recall");
+	    String precision = (String) paragraphAgreementEntity.getProperty("precision");
+
+	    String coderName = (String) result.getProperty("name");
+
+	    result.setProperty("reportRow", coderName + "," + fmeasure + "," + recall + "," + precision);
+	    result.removeProperty("name");
+	    result.removeProperty("paragraphAgreement");
+	    //TODO das ParagraphAgreement selbst muss man hier noch löschen! (oder passiert das automatisch?)
+
 	}
+	//Persist all changes at once
+	datastore.put(validationResults); //Funktioniert das so?
     }
 
-    private void migrateParagraphAgreements(User user) {
+    private void migrateParagraphAgreements(User user, DatastoreService datastore) {
 	//TODO all ParagraphAgreement -> TabularValidationReportRow
+	//As the Entity name changed this method looks different from the ones above!
 	//HINT: as the class ParagraphAgreement has been renamed to FMeasureResult we need to access it manually in the DataStore.
-	for (Entity result : getAll("ParagraphAgreement")) {
+	for (Entity result : getAll("ParagraphAgreement", datastore)) {
 	    String fmeasure = (String) result.getProperty("fmeasure");
 	    String recall = (String) result.getProperty("recall");
 	    String precision = (String) result.getProperty("precision");
@@ -94,7 +140,7 @@ public class DataMigrationEndpoint {
 	    cells.add(recall);
 	    cells.add(precision);
 	    TabularValidationReportRow newRow = new TabularValidationReportRow(cells);
-	    //TODO Key?
+	    //TODO Key? kann ich den überhaupt manuell setzen? Das ist doch immer die Sache des DataStores.
 	    //TODO persist newRow
 	}
     }
@@ -116,8 +162,7 @@ public class DataMigrationEndpoint {
      * @param entityName the String name of the entity in the DataStore
      * @return all Entities from the DataStore as Iterable
      */
-    private Iterable<Entity> getAll(String entityName) {
-	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    private Iterable<Entity> getAll(String entityName, DatastoreService datastore) {
 	com.google.appengine.api.datastore.Query q = new com.google.appengine.api.datastore.Query(entityName);
 	PreparedQuery pq = datastore.prepare(q);
 
