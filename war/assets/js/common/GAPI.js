c6937d62220559b658763a9af82e91841c6390d0
Andreas <andreas.kaufmann@fau.de>
2017-05-26 10:56:57 +0200

    Reverted App ID and Client ID

--- war/assets/js/common/GAPI.js
+++ war/assets/js/common/GAPI.js
@@ -1,12 +1,8 @@
 import Account from './Account.jsx';
 
 var scopes = 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
-//from: https://console.cloud.google.com/apis/credentials?project=qdacity-quality-metrics 
-var client_id = '98628102717-19mnq0ntljj8gptvs3fh5v51t5dqj32j.apps.googleusercontent.com';
-var api_version = 'v5';
-//var api_path = 'https://qdacity-quality-metrics.appspot.com/_ah/api';
-//var api_path = 'https://4-dot-qdacity-quality-metrics-app.appspot.com/_ah/api';
-var api_path = 'http://localhost:8888/_ah/api';
+var client_id = '309419937441-6d41vclqvedjptnel95i2hs4hu75u4v7.apps.googleusercontent.com';
+var api_path = 'https://5-dot-qdacity-app.appspot.com/_ah/api'; //'https://localhost:8888/_ah/api' var api_version = 'v5';
 
 export default function loadGAPIs(allLoadedCallback) {
 	var _this = this;
