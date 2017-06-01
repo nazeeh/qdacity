8956a813f50ca281ad7725c9502139fd036795b8
Matthias Schoepe <matthias.schoepe@fau.de>
2017-06-01 15:35:39 +0200

    Adding new Attribute to ValidationReport and introducing toString for EvaluationMethod

--- src/com/qdacity/project/metrics/EvaluationMethod.java
+++ src/com/qdacity/project/metrics/EvaluationMethod.java
@@ -5,6 +5,10 @@ public enum EvaluationMethod {
     KRIPPENDORFFS_ALPHA,
     FLEISS_KAPPA;
 
+    private static final String FMEASURESTRING = "f-measure";
+    private static final String KRIPPSALPHASTRING = "krippendorffs-alpha";
+    private static final String FLEISSKAPPASTRING = "fleiss-kappa";
+
     /**
      * returns an Objekt of this Type matching the given String. If it does not
      * match to any, always F_MEASURE is returned as default.
@@ -14,17 +18,30 @@ public enum EvaluationMethod {
      * @return an EvaluationMethod
      */
     public static EvaluationMethod fromString(String evalMethod) {
-        switch (evalMethod) {
-            case "f-measure":
-                return EvaluationMethod.F_MEASURE;
-            case "krippendorffs-alpha":
-                return EvaluationMethod.KRIPPENDORFFS_ALPHA;
-            case "fleiss-kappa":
-                return EvaluationMethod.FLEISS_KAPPA;
-            default:
-                return EvaluationMethod.F_MEASURE;
+	switch (evalMethod) {
+	    case FMEASURESTRING:
+		return EvaluationMethod.F_MEASURE;
+	    case KRIPPSALPHASTRING:
+		return EvaluationMethod.KRIPPENDORFFS_ALPHA;
+	    case FLEISSKAPPASTRING:
+		return EvaluationMethod.FLEISS_KAPPA;
+	    default:
+		return EvaluationMethod.F_MEASURE;
 
-        }
+	}
+    }
+
+    public String toString() {
+	switch (this) {
+	    case F_MEASURE:
+		return FMEASURESTRING;
+	    case KRIPPENDORFFS_ALPHA:
+		return KRIPPSALPHASTRING;
+	    case FLEISS_KAPPA:
+		return FLEISSKAPPASTRING;
+	    default:
+		return "UNKNOWN";
+	}
     }
 
 }
