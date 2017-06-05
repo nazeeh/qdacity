1fc7b4c04af92871e9319790be56872bfbe08b73
Felix Loos <loos.felix93@gmail.com>
2017-06-05 19:21:14 +0200

    Implemented javascript UmlCodePosition endpoint

---
+++ war/assets/js/common/endpoints/UmlCodePositionEndpoint.js
@@ -0,0 +1,26 @@
+import Promisizer from './Promisizer'
+
+export default class CodePositionEndpoint {
+	constructor() {}
+
+	static listCodePositions(codeSystemId) {
+		var apiMethod = gapi.client.qdacity.umlCodePosition.listCodePositions({
+			'codeSystemId': codeSystemId
+		});
+		return Promisizer.makePromise(apiMethod);
+	}
+
+	static insertCodePositions(umlCodePositions) {
+		var apiMethod = gapi.client.qdacity.umlCodePosition.insertCodePositions({
+			'umlCodePositions': umlCodePositions
+		});
+		return Promisizer.makePromise(apiMethod);
+	}
+
+	static updateCodePositions(umlCodePositions) {
+		var apiMethod = gapi.client.qdacity.umlCodePosition.updateCodePositions({
+			'umlCodePositions': umlCodePositions
+		});
+		return Promisizer.makePromise(apiMethod);
+	}
+}
