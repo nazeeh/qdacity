49be051ec9c242f943488e4176554d03c0abcdec
Felix Loos <loos.felix93@gmail.com>
2017-06-13 10:40:03 +0200

    Implemented simple toolbar

---
+++ war/assets/js/pages/uml-editor/Toolbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default class Toolbar extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.umlEditorView = this.props.umlEditorView;
+	}
+
+	getStyles() {
+		return {
+			container: {
+				padding: '8px 20px 8px 20px',
+				borderBottom: '1px solid #808080'
+			},
+			buttonZoomOut: {
+				marginLeft: '10px'
+			}
+		};
+	}
+
+	render() {
+		const _this = this;
+
+		const styles = this.getStyles();
+
+		return (
+			<div style={styles.container}>
+                <button type="button" className="btn btn-default"><i className="fa fa-search-plus"></i></button>
+                <button style={styles.buttonZoomOut} type="button" className="btn btn-default"><i className="fa fa-search-minus"></i></button>
+            </div>
+		);
+	}
+
+}
