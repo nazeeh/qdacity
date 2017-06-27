a2915d336a6f1c740b56d96c152c8f4629a6357c
Matthias Schoepe <matthias.schoepe@fau.de>
2017-06-27 12:15:37 +0200

    SaturationCalculator DRAFT

--- src/com/qdacity/project/saturation/SaturationCalculator.java
+++ src/com/qdacity/project/saturation/SaturationCalculator.java
@@ -10,11 +10,11 @@ import java.util.Date;
 import java.util.List;
 
 public class SaturationCalculator {
-    
+
     private final Long projectId;
     private final SaturationParameters params;
     private final List<Change> changes;
-    
+
     public SaturationCalculator(Long projectId) throws UnauthorizedException {
 	this.projectId = projectId;
 	SaturationEndpoint se = new SaturationEndpoint();
@@ -22,21 +22,21 @@ public class SaturationCalculator {
 	//TODO get CHanges ab Zeitpunkt X! hier sind es immer alle changes!
 	this.changes = new ChangeEndpoint().getAllChanges(projectId);
     }
-    
+
     public SaturationResult calculateSaturation() {
 	SaturationResult result = new SaturationResult();
 	result.setProjectId(projectId);
 	result.setSaturationParameters(params);
-	
+
 	double documentSaturation = calculateDocumentSaturation();
 	result.setDocumentSaturation(documentSaturation);
 	//documentSaturation is not weighted by itself!
 	result.setDocumentSaturation(documentSaturation);
-	
+
 	//TODO weighting
 	double codeSaturation = calculateCodeSaturation();
 	result.setCodeSaturation(codeSaturation);
-	
+
 	//TODO
 	double totalSaturation = 0.0; //TODO needs weighting!
 	result.setTotalSaturation(totalSaturation);
@@ -44,7 +44,7 @@ public class SaturationCalculator {
 	result.setCreationTime(new Date(System.currentTimeMillis()));
 	return result;
     }
-    
+
     private double calculateDocumentSaturation() {
 	double numberOfNewDocuments = 0.0;
 	for (Change change : changes) {
@@ -54,14 +54,41 @@ public class SaturationCalculator {
 	}
 	double totalNumberOfDocuments = TextDocumentEndpoint.countDocuments(projectId);
 	double numberOfDocumentsBeforeChange = totalNumberOfDocuments - numberOfNewDocuments;
-	
+
 	return 1.0 - (numberOfNewDocuments / numberOfDocumentsBeforeChange);
-	
+
     }
-    
+
     private double calculateCodeSaturation() {
-	//TODO
+	double numNewCodes = 0.0;
+	double numDeletedCodes = 0.0;
+	for (Change change : changes) {
+	    switch(change.getObjectType()) {
+		case CODE:
+		    switch(change.getChangeType()) {
+			case CREATED:
+			    numNewCodes += 1.0;
+			    break;
+			case MODIFIED:
+			    //TODO
+			    break;
+			    //TODO wie mit changes an codes umgehen, wenn der code geändert und dann gelöscht wurde?
+			case DELETED:
+			    numDeletedCodes += 1.0;
+			    break;
+		    }
+		    break;
+		case CODEBOOK_ENTRY:
+		    //TODO
+		    break;
+		    
+	    }
+
+	}
+	double numCurrentCodes = 0.0; //TODO get from CodeEnpoint
+	double totalNumberOfCodesBeforeChanges = numCurrentCodes - numNewCodes + numDeletedCodes;
+
 	return 0.0;
     }
-    
+
 }
