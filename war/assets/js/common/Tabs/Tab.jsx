3eb4c4362aa790291902c23824fb76f380ba7b86
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-06-29 13:23:01 +0200

    Added styling to tabs

--- war/assets/js/common/Tabs/Tab.jsx
+++ war/assets/js/common/Tabs/Tab.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const StyledTab = styled.div `
+    padding: 5px 5px 5px 5px;
+	font-size: 15px;
+	font-weight: bold;
+	cursor: pointer;
+	color: ${props => props.isActive ?'#fff' : '#000'};
+	background-color: ${props => props.isActive ?'#337ab7' : ''};
+	border-bottom-style: solid;
+	border-bottom-color:  #337ab7;
+`;
 
 export default class Tab extends React.Component {
 	constructor(props) {
@@ -10,9 +22,9 @@ export default class Tab extends React.Component {
 
 	render(){
 		return(
-			<div onClick={() => this.props.changeTab(this.props.tabIndex)}>
+			<StyledTab isActive={this.props.isActive} onClick={() => this.props.changeTab(this.props.tabIndex)}>
 				{this.props.tabTitle}
-			</div>
+			</StyledTab>
 		);
 	}
 }
