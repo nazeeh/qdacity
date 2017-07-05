b3449a2f67375c1a2764e22f774c41452fe4098a
Matthias Schoepe <matthias.schoepe@fau.de>
2017-07-05 19:34:55 +0200

    Correcting errornous weighted average calculation of saturation on project dashboard

--- war/assets/js/pages/project-dashboard/ProjectStats.jsx
+++ war/assets/js/pages/project-dashboard/ProjectStats.jsx
@@ -30,27 +30,44 @@ export default class ProjectStats extends React.Component {
         calculateAvgSaturation() {
             var sr = this.state.saturation;
             var pr = sr.saturationParameters;
-            var cf = 10; //correctiveFactor as the weights are between 0 and 1
             if(pr != undefined) {
-                var sum = sr.applyCodeSaturation * ( pr.appliedCodesChangeWeight * cf )
-                            + sr.deleteCodeRelationShipSaturation * ( pr.deleteCodeRelationShipChangeWeight * cf )
-                            + sr.deleteCodeSaturation * ( pr.deleteCodeChangeWeight * cf )
-                            + sr.documentSaturation * ( pr.insertDocumentChangeWeight * cf )
-                            + sr.insertCodeRelationShipSaturation * ( pr.insertCodeRelationShipChangeWeight * cf )
-                            + sr.insertCodeSaturation * ( pr.insertCodeChangeWeight * cf )
-                            + sr.relocateCodeSaturation * ( pr.relocateCodeChangeWeight * cf )
-                            + sr.updateCodeAuthorSaturation * ( pr.updateCodeAuthorChangeWeight * cf )
-                            + sr.updateCodeBookEntryDefinitionSaturation * ( pr.updateCodeBookEntryDefinitionChangeWeight * cf )
-                            + sr.updateCodeBookEntryExampleSaturation * ( pr.updateCodeBookEntryExampleChangeWeight * cf )
-                            + sr.updateCodeBookEntryShortDefinitionSaturation * ( pr.updateCodeBookEntryShortDefinitionChangeWeight * cf )
-                            + sr.updateCodeBookEntryWhenNotToUseSaturation * ( pr.updateCodeBookEntryWhenNotToUseChangeWeight * cf )
-                            + sr.updateCodeBookEntryWhenToUseSaturation * ( pr.updateCodeBookEntryWhenToUseChangeWeight * cf )
-                            + sr.updateCodeColorSaturation * ( pr.updateCodeColorChangeWeight * cf )
-                            + sr.updateCodeIdSaturation * ( pr.updateCodeIdChangeWeight * cf )
-                            + sr.updateCodeMemoSaturation * ( pr.updateCodeMemoChangeWeight * cf )
-                            + sr.updateCodeNameSaturation * ( pr.updateCodeNameChangeWeight * cf);
-                var avg = sum / ( 17 * cf);
-                return avg+"";
+                //weights are set beween 0 and 1 and are > 1 in total. 
+                // we need to normalize them when calculating the weighted average
+                var sumParameters = pr.appliedCodesChangeWeight
+                                    + pr.deleteCodeRelationShipChangeWeight
+                                    + pr.deleteCodeChangeWeight
+                                    + pr.insertDocumentChangeWeight
+                                    + pr.insertCodeRelationShipChangeWeight
+                                    + pr.insertCodeChangeWeight
+                                    + pr.relocateCodeChangeWeight
+                                    + pr.updateCodeAuthorChangeWeight
+                                    + pr.updateCodeBookEntryDefinitionChangeWeight
+                                    + pr.updateCodeBookEntryExampleChangeWeight
+                                    + pr.updateCodeBookEntryShortDefinitionChangeWeight
+                                    + pr.updateCodeBookEntryWhenNotToUseChangeWeight
+                                    + pr.updateCodeBookEntryWhenToUseChangeWeight
+                                    + pr.updateCodeColorChangeWeight
+                                    + pr.updateCodeIdChangeWeight
+                                    + pr.updateCodeMemoChangeWeight
+                                    + pr.updateCodeNameChangeWeight;
+                var weightedAvg = sr.applyCodeSaturation * ( pr.appliedCodesChangeWeight / sumParameters )
+                            + sr.deleteCodeRelationShipSaturation * ( pr.deleteCodeRelationShipChangeWeight / sumParameters )
+                            + sr.deleteCodeSaturation * ( pr.deleteCodeChangeWeight / sumParameters )
+                            + sr.documentSaturation * ( pr.insertDocumentChangeWeight / sumParameters )
+                            + sr.insertCodeRelationShipSaturation * ( pr.insertCodeRelationShipChangeWeight / sumParameters )
+                            + sr.insertCodeSaturation * ( pr.insertCodeChangeWeight / sumParameters )
+                            + sr.relocateCodeSaturation * ( pr.relocateCodeChangeWeight / sumParameters )
+                            + sr.updateCodeAuthorSaturation * ( pr.updateCodeAuthorChangeWeight / sumParameters )
+                            + sr.updateCodeBookEntryDefinitionSaturation * ( pr.updateCodeBookEntryDefinitionChangeWeight / sumParameters )
+                            + sr.updateCodeBookEntryExampleSaturation * ( pr.updateCodeBookEntryExampleChangeWeight / sumParameters )
+                            + sr.updateCodeBookEntryShortDefinitionSaturation * ( pr.updateCodeBookEntryShortDefinitionChangeWeight / sumParameters )
+                            + sr.updateCodeBookEntryWhenNotToUseSaturation * ( pr.updateCodeBookEntryWhenNotToUseChangeWeight / sumParameters )
+                            + sr.updateCodeBookEntryWhenToUseSaturation * ( pr.updateCodeBookEntryWhenToUseChangeWeight / sumParameters )
+                            + sr.updateCodeColorSaturation * ( pr.updateCodeColorChangeWeight / sumParameters )
+                            + sr.updateCodeIdSaturation * ( pr.updateCodeIdChangeWeight / sumParameters )
+                            + sr.updateCodeMemoSaturation * ( pr.updateCodeMemoChangeWeight / sumParameters )
+                            + sr.updateCodeNameSaturation * ( pr.updateCodeNameChangeWeight / sumParameters);
+                return weightedAvg+"";
           } else {
               return "N/A";
           }
