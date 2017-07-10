36c76b7deb69119d69fcae664530f4547722c57c
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-07-10 13:41:24 +0200

    Refactored SettingsBtn.jsx using styled-components

--- war/assets/js/pages/project-dashboard/TitleRow/SettingsBtn.jsx
+++ war/assets/js/pages/project-dashboard/TitleRow/SettingsBtn.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import ProjectEndpoint from '../../../common/endpoints/ProjectEndpoint';
 import Settings from '../../../common/modals/Settings';
 
+const StyledSettingsBtn = styled.button `
+	margin-left: 5px;
+`;
 
 export default class SettingsBtn extends React.Component {
 	constructor(props) {
@@ -12,14 +15,6 @@ export default class SettingsBtn extends React.Component {
 			umlEditor: this.props.umlEditor
 		};
 		this.showSettingsModal = this.showSettingsModal.bind(this);
-	}
-
-	getStyles() {
-		return {
-			settingsBtn: {
-				marginLeft: "5px"
-			}
-		};
 	}
 
 	setUmlEditor(pUseUmlEditor) {
@@ -49,17 +44,14 @@ export default class SettingsBtn extends React.Component {
 	render() {
 		if (!this.props.isProjectOwner) return null;
 
-		const styles = this.getStyles();
-
 		return (
-			<button 
-				type="button" 
+			<StyledSettingsBtn
+				type="button"
 				className="btn btn-default btn-sm pull-right"
-				style={styles.settingsBtn}
 				onClick={this.showSettingsModal}
 			>
 				<i className="fa fa-cog fa-lg"></i>&nbsp;&nbsp;<b>Settings</b>
-			</button>
+			</StyledSettingsBtn>
 		);
 	}
 
