b6f1a30812c8a0db5a4ca05133caefcbe9c7adb2
Matthias Schoepe <matthias.schoepe@fau.de>
2017-07-11 10:37:05 +0200

    Styling Saturation Line Diagramm (more space)

--- war/assets/js/common/modals/SaturationModal.js
+++ war/assets/js/common/modals/SaturationModal.js
@@ -9,7 +9,7 @@ export default class SaturationModal extends VexModal {
         this.formElements = '';
         this.projectId = projectId;
         this.results;
-        this.formElements += '<div id="saturation" style="text-align: center; background-color: #eee; font-color:#222; overflow:hidden; overflow-x: scroll;"><div id="saturationMetaData"></div><table cellpadding="0" cellspacing="0" border="0" class="display" id="saturationTable"></table><div id="saturationChart"></div><div id="loadingAnimation" class="centerParent"><div id="reactLoading" class="centerChild"></div></div></div>';
+        this.formElements += '<div id="saturation" style="text-align: center; background-color: #eee; font-color:#222; overflow:hidden; overflow-x: scroll;"><div id="saturationMetaData"></div><table cellpadding="0" cellspacing="0" border="0" class="display" id="saturationTable"></table><div style="height: 350px;" id="saturationChart"></div><div id="loadingAnimation" class="centerParent"><div id="reactLoading" class="centerChild"></div></div></div>';
     }
 
     showModal() {
