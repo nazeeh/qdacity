a6290a5ff37457bbe5fa249029d5d441a4276fbc
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-07-12 18:13:11 +0200

    Removed dependency from react-loading Instead put SVG directly in ReactLoading.jsx Also updated version of styled-components

--- war/assets/js/common/ReactLoading.jsx
+++ war/assets/js/common/ReactLoading.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import Loading from 'react-loading';
+import styled from 'styled-components';
+
+const StyledLoagingAnimation = styled.div `
+	height: 64px;
+	width: 64px;
+	fill: ${props => props.color ? props.color : '#fff'};
+`;
 
 export default class ReactLoading extends React.Component {
-
-
 	constructor(props) {
 		super(props);
 		this.state = {
 			show: false
 		};
-		if (typeof this.props.color != 'undefined') this.color = this.props.color;
-		else this.color = '#e3e3e3';
 	}
 
 	show() {
@@ -23,7 +25,25 @@ export default class ReactLoading extends React.Component {
 
 		//if(this.state.show) {
 		if (true) {
-			return <Loading type='bars' color={this.color} />;
+			return <StyledLoagingAnimation color={this.props.color}>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+				  <path transform="translate(2)" d="M0 12 V20 H4 V12z">
+				    <animate attributeName="d" values="M0 12 V20 H4 V12z; M0 4 V28 H4 V4z; M0 12 V20 H4 V12z; M0 12 V20 H4 V12z" dur="1.2s" repeatCount="indefinite" begin="0" keyTimes="0;.2;.5;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.8 0.4 0.8" calcMode="spline"  />
+				  </path>
+				  <path transform="translate(8)" d="M0 12 V20 H4 V12z">
+				    <animate attributeName="d" values="M0 12 V20 H4 V12z; M0 4 V28 H4 V4z; M0 12 V20 H4 V12z; M0 12 V20 H4 V12z" dur="1.2s" repeatCount="indefinite" begin="0.2" keyTimes="0;.2;.5;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.8 0.4 0.8" calcMode="spline"  />
+				  </path>
+				  <path transform="translate(14)" d="M0 12 V20 H4 V12z">
+				    <animate attributeName="d" values="M0 12 V20 H4 V12z; M0 4 V28 H4 V4z; M0 12 V20 H4 V12z; M0 12 V20 H4 V12z" dur="1.2s" repeatCount="indefinite" begin="0.4" keyTimes="0;.2;.5;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.8 0.4 0.8" calcMode="spline" />
+				  </path>
+				  <path transform="translate(20)" d="M0 12 V20 H4 V12z">
+				    <animate attributeName="d" values="M0 12 V20 H4 V12z; M0 4 V28 H4 V4z; M0 12 V20 H4 V12z; M0 12 V20 H4 V12z" dur="1.2s" repeatCount="indefinite" begin="0.6" keyTimes="0;.2;.5;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.8 0.4 0.8" calcMode="spline" />
+				  </path>
+				  <path transform="translate(26)" d="M0 12 V20 H4 V12z">
+				    <animate attributeName="d" values="M0 12 V20 H4 V12z; M0 4 V28 H4 V4z; M0 12 V20 H4 V12z; M0 12 V20 H4 V12z" dur="1.2s" repeatCount="indefinite" begin="0.8" keyTimes="0;.2;.5;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.8 0.4 0.8" calcMode="spline" />
+				  </path>
+				</svg>
+			</StyledLoagingAnimation>;
 		} else {
 			return null;
 		}
