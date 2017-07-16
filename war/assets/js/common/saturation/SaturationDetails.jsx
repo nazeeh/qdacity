b86b8ab207f09d7cd6ac8584a01a60688aadbfa4
Matthias Schoepe <matthias.schoepe@fau.de>
2017-07-16 17:18:35 +0200

    Refactoring of SaturationDetails with new SaturationWeights

--- war/assets/js/common/saturation/SaturationDetails.jsx
+++ war/assets/js/common/saturation/SaturationDetails.jsx
@@ -1,4 +1,5 @@
 import 'script!../../../../components/DataTables-1.10.7/media/js/jquery.dataTables.min.js';
+import SaturationWeights from '../saturation/SaturationWeights'
 
 export default class SaturationDetails extends React.Component {
     constructor(props) {
@@ -9,10 +10,10 @@ export default class SaturationDetails extends React.Component {
         this.initTable();
     }
     componentDidUpdate() {
-	    if (this.props.saturation){
-	    	this.initTable();
-	        this.drawDataTable();
-	    }
+        if (this.props.saturation) {
+            this.initTable();
+            this.drawDataTable();
+        }
     }
 
     initTable() {
@@ -40,32 +41,18 @@ export default class SaturationDetails extends React.Component {
             ]
 
         });
-}
+    }
 
     drawDataTable() {
         if (typeof this.props.saturation !== 'undefined') {
             var table = $('#saturationTable').DataTable();
             table.clear();
 
-            var pr = this.props.saturation.saturationParameters;
-            table.row.add(['Applied Codes', this.toPercent(this.props.saturation.applyCodeSaturation), this.toPercent(pr.appliedCodesChangeWeight)]);
-            table.row.add(['Deleted Code Relationships', this.toPercent(this.props.saturation.deleteCodeRelationShipSaturation), this.toPercent(pr.deleteCodeRelationShipChangeWeight)]);
-            table.row.add(['Deleted Codes', this.toPercent(this.props.saturation.deleteCodeSaturation), this.toPercent(pr.deleteCodeChangeWeight)]);
-            table.row.add(['New Documents', this.toPercent(this.props.saturation.documentSaturation), this.toPercent(pr.insertDocumentChangeWeight)]);
-            table.row.add(['New Code Relationships', this.toPercent(this.props.saturation.insertCodeRelationShipSaturation), this.toPercent(pr.insertCodeRelationShipChangeWeight)]);
-            table.row.add(['New Codes', this.toPercent(this.props.saturation.insertCodeSaturation), this.toPercent(pr.insertCodeChangeWeight)]);
-            table.row.add(['Relocated Codes', this.toPercent(this.props.saturation.relocateCodeSaturation), this.toPercent(pr.relocateCodeChangeWeight)]);
-            table.row.add(['Code Author Changes', this.toPercent(this.props.saturation.updateCodeAuthorSaturation), this.toPercent(pr.updateCodeAuthorChangeWeight)]);
-            table.row.add(['CodeBookEntry Definition Changes', this.toPercent(this.props.saturation.updateCodeBookEntryDefinitionSaturation), this.toPercent(pr.updateCodeBookEntryDefinitionChangeWeight)]);
-            table.row.add(['CodeBookEntry Example Changes', this.toPercent(this.props.saturation.updateCodeBookEntryExampleSaturation), this.toPercent(pr.updateCodeBookEntryExampleChangeWeight)]);
-            table.row.add(['CodeBookEntry Short Definition Changes', this.toPercent(this.props.saturation.updateCodeBookEntryShortDefinitionSaturation), this.toPercent(pr.updateCodeBookEntryShortDefinitionChangeWeight)]);
-            table.row.add(['CodeBookEntry When Not To Use Changes', this.toPercent(this.props.saturation.updateCodeBookEntryWhenNotToUseSaturation), this.toPercent(pr.updateCodeBookEntryWhenNotToUseChangeWeight)]);
-            table.row.add(['CodeBookEntry When To Use Changes', this.toPercent(this.props.saturation.updateCodeBookEntryWhenToUseSaturation), this.toPercent(pr.updateCodeBookEntryWhenToUseChangeWeight)]);
-            table.row.add(['Code Color Changes', this.toPercent(this.props.saturation.updateCodeColorSaturation), this.toPercent(pr.updateCodeColorChangeWeight)]);
-            //we skip id changes here!
-            table.row.add(['Code Memo Changes', this.toPercent(this.props.saturation.updateCodeMemoSaturation), this.toPercent(pr.updateCodeMemoChangeWeight)]);
-            table.row.add(['Code Name Changes', this.toPercent(this.props.saturation.updateCodeNameSaturation), this.toPercent(pr.updateCodeNameChangeWeight)]);
+            var saturationNameAndWeightsAndSaturation = new SaturationWeights(this.props.saturation.saturationParameters).getNameAndWeightsAndSaturationArray(this.props.saturation);
 
+            for (var i in saturationNameAndWeightsAndSaturation) {
+                table.row.add([saturationNameAndWeightsAndSaturation[i][0], this.toPercent(saturationNameAndWeightsAndSaturation[i][1]), this.toPercent(saturationNameAndWeightsAndSaturation[i][2])]);
+            }
             table.draw();
         }
     }
@@ -75,14 +62,15 @@ export default class SaturationDetails extends React.Component {
     }
 
     render() {
-    	if (!this.props.saturation) return null;
+        if (!this.props.saturation)
+            return null;
         return(<div>
-            <p>Last calculation of saturation is from: {this.props.saturation.evalStartDate} to {this.props.saturation.creationTime}</p>
-            <table id="saturationTable" className="display">
-        
-            </table>
-            <p>Parameters used from : {this.props.saturation.saturationParameters.creationTime}</p>
-        </div>);
+                <p>Last calculation of saturation is from: {this.props.saturation.evalStartDate} to {this.props.saturation.creationTime}</p>
+                <table id="saturationTable" className="display">
+
+                </table>
+                <p>Parameters used from : {this.props.saturation.saturationParameters.creationTime}</p>
+                </div>);
     }
 
 }
--- war/assets/js/common/saturation/SaturationWeights.js
+++ war/assets/js/common/saturation/SaturationWeights.js
@@ -3,7 +3,7 @@ export default class SaturationWeights {
         this.saturationParameters = saturationParameters;
     }
 
-    getRepresentationArray() {
+    getNameAndWeightsArray() {
         var sp = this.saturationParameters;
         var saturationWeights = [
             ['Applied Codes', sp.appliedCodesChangeWeight],
@@ -25,5 +25,27 @@ export default class SaturationWeights {
         ];
 
         return saturationWeights;
+    }
+
+    getNameAndWeightsAndSaturationArray(saturation) {
+        var nameAndWeights = this.getNameAndWeightsArray();
+        nameAndWeights[0] = nameAndWeights[0].concat(saturation.applyCodeSaturation);
+        nameAndWeights[1] = nameAndWeights[1].concat(saturation.deleteCodeRelationShipSaturation);
+        nameAndWeights[2] = nameAndWeights[2].concat(saturation.deleteCodeSaturation);
+        nameAndWeights[3] = nameAndWeights[3].concat(saturation.documentSaturation);
+        nameAndWeights[4] = nameAndWeights[4].concat(saturation.insertCodeRelationShipSaturation);
+        nameAndWeights[5] = nameAndWeights[5].concat(saturation.insertCodeSaturation);
+        nameAndWeights[6] = nameAndWeights[6].concat(saturation.relocateCodeSaturation);
+        nameAndWeights[7] = nameAndWeights[7].concat(saturation.updateCodeAuthorSaturation);
+        nameAndWeights[8] = nameAndWeights[8].concat(saturation.updateCodeBookEntryDefinitionSaturation);
+        nameAndWeights[9] = nameAndWeights[9].concat(saturation.updateCodeBookEntryExampleSaturation);
+        nameAndWeights[10] = nameAndWeights[10].concat(saturation.updateCodeBookEntryShortDefinitionSaturation);
+        nameAndWeights[11] = nameAndWeights[11].concat(saturation.updateCodeBookEntryWhenNotToUseSaturation);
+        nameAndWeights[12] = nameAndWeights[12].concat(saturation.updateCodeBookEntryWhenToUseSaturation);
+        nameAndWeights[13] = nameAndWeights[13].concat(saturation.updateCodeColorSaturation);
+        nameAndWeights[14] = nameAndWeights[14].concat(saturation.updateCodeMemoSaturation);
+        nameAndWeights[15] = nameAndWeights[15].concat(saturation.updateCodeNameSaturation);
+
+        return nameAndWeights;
     }
 }
