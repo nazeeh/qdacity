b6d15c60478c647e3081954ecfca15f909058212
Matthias Schoepe <matthias.schoepe@fau.de>
2017-07-16 13:14:21 +0200

    first draft of new Settings for Saturation

--- war/assets/js/common/modals/Settings.js
+++ war/assets/js/common/modals/Settings.js
@@ -17,9 +17,40 @@ export default class Settings extends VexModal {
 
 				var checked = '';
 				if (umlEditorEnabled) checked = ' checked';
+                                
+                                var saturationWeights = [
+                                  'Applied Codes',
+                                  'Deleted Code Relationships',
+                                  'Deleted Codes',
+                                  'New Documents',
+                                  'New Code Relationships',
+                                  'New Codes',
+                                  'Relocated Codes',
+                                  'Code Author Changes',
+                                  'CodeBookEntry Definition Changes',
+                                  'CodeBookEntry Example Changes',
+                                  'CodeBookEntry Short Definition Changes',
+                                  'CodeBookEntry When Not To Use Changes',
+                                  'CodeBookEntry When To Use Changes',
+                                  'Code Color Changes',
+                                  'Code Memo Changes',
+                                  'Code Name Changes'
+                                ];
 
 				formElements += '<div class="checkbox">';
 				formElements += '<label><input id="settingsUmlEditorEnabled" type="checkbox" value=""' + checked + '>Enable UML Editor</label>';
+				formElements += '</div>';
+				formElements += '<div>';
+				formElements += '<p><b>Saturation Configuration</b></p>';
+				formElements += '<p>Default interval for saturation: <input id="interval" type="number" value="-1" min="1" max="20" style="width: 60px;"/> revisions</p>';
+				formElements += '<table><tr><th>Change</th><th>Weight in %</th><th>Saturation at XX%</th></tr>';
+                                for(var i in saturationWeights) {
+                                    formElements += '<tr>';
+                                    //TODO
+                                    formElements += '<td>'+saturationWeights[i]+'</td><td><input id="interval" type="number" value="-1" min="0" max="100" style="width: 60px;"/>%</td><td><input id="interval" type="number" value="-1" min="0" max="100" style="width: 60px;"/>%</td>';
+                                    formElements += '</tr>';
+                                }
+				formElements += '</table>';
 				formElements += '</div>';
 
 				formElements += '<br>';
