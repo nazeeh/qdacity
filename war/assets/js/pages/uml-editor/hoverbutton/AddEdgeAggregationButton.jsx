2ae8c5177b9de8e8ec7e4d63cb1904566c3f09f2
Felix Loos <loos.felix93@gmail.com>
2017-08-01 13:37:54 +0200

    Fixed horizontal position for hover buttons

--- war/assets/js/pages/uml-editor/hoverbutton/AddEdgeAggregationButton.jsx
+++ war/assets/js/pages/uml-editor/hoverbutton/AddEdgeAggregationButton.jsx
@@ -30,7 +30,7 @@ export default class AddEdgeAggregationButton extends ImageHoverButton {
 		const offsetToNode = 6;
 		const offsetToButton = 6;
 
-		const x = this.props.x + this.props.width + offsetToNode + sizeX * this.props.scale + offsetToButton * this.props.scale;
+		const x = this.props.x + this.props.width + offsetToNode * this.props.scale + sizeX * this.props.scale + offsetToButton * this.props.scale;
 		const y = this.props.y + offsetTop - offsetToButton * this.props.scale - sizeY * this.props.scale;
 		const width = sizeX * this.props.scale;
 		const height = sizeY * this.props.scale;
--- war/assets/js/pages/uml-editor/hoverbutton/AddEdgeAssociationButton.jsx
+++ war/assets/js/pages/uml-editor/hoverbutton/AddEdgeAssociationButton.jsx
@@ -30,7 +30,7 @@ export default class AddEdgeAssociationButton extends ImageHoverButton {
 		const offsetToNode = 6;
 		const offsetToButton = 6;
 
-		const x = this.props.x + this.props.width + offsetToNode + sizeX * this.props.scale + offsetToButton * this.props.scale;
+		const x = this.props.x + this.props.width + offsetToNode * this.props.scale + sizeX * this.props.scale + offsetToButton * this.props.scale;
 		const y = this.props.y + offsetTop;
 		const width = sizeX * this.props.scale;
 		const height = sizeY * this.props.scale;
