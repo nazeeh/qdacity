98c0dedc55bce3b94d09c706da751e46cd6adab1
Felix Loos <loos.felix93@gmail.com>
2017-08-01 13:21:45 +0200

    Fixed addEdgeButton offset

--- war/assets/js/pages/uml-editor/hoverbutton/AddEdgeButton.jsx
+++ war/assets/js/pages/uml-editor/hoverbutton/AddEdgeButton.jsx
@@ -27,7 +27,7 @@ export default class AddEdgeButton extends ImageHoverButton {
 
 		const offsetToNode = 6;
 
-		const x = this.props.x + this.props.width + offsetToNode;
+		const x = this.props.x + this.props.width + offsetToNode * this.props.scale;
 		const y = this.props.y + offsetTop;
 		const width = sizeX * this.props.scale;
 		const height = sizeY * this.props.scale;
