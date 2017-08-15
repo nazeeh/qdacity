4333780d045037a11a262147c87a1e142118a627
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-08-15 16:11:09 +0200

    Used new style on CodingEditorBtn.jsx and CreateRevisionBtn.jsx

--- war/assets/js/pages/project-dashboard/TitleRow/CodingEditorBtn.jsx
+++ war/assets/js/pages/project-dashboard/TitleRow/CodingEditorBtn.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledSettingsBtn = styled.button `
+import BtnDefault from '../../../common/styles/Btn.jsx';
+
+const StyledSettingsBtn = BtnDefault.extend  `
 	margin-left: 5px;
 	& > i {
     	padding-right: 5px;
