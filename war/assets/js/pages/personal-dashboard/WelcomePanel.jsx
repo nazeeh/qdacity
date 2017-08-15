c76e19f7fe0dc949feabbde5b4eb6a63ce6f7f89
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-08-15 13:10:14 +0200

    Removed strange styling on support email on personal dashboard

--- war/assets/js/pages/personal-dashboard/WelcomePanel.jsx
+++ war/assets/js/pages/personal-dashboard/WelcomePanel.jsx
@@ -11,7 +11,7 @@ export default class WelcomePanel extends React.Component {
 				<h1>Welcome {this.props.account.getProfile().getGivenName()}</h1>
 				<p>QDAcity is currently in beta. We appreciate feedback to
 					<a href="mailto:support@qdacity.com?Subject=QDAcity%20support">
-						<span className="network-name">support@qdacity.com</span>
+						<span> support@qdacity.com</span>
 					</a>.
 				</p>
 				<p>For NYT WS2017 at FAU please request access to <a href="project-dashboard.html?project=6263559650541568&amp;type=PROJECT">this project</a> by clicking "Re-Code" for a specific revision. You will then need to wait for authorization.</p>
