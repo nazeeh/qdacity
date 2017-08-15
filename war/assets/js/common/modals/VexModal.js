a7b2a5da18aa9d2495ffccf94cce052126e9dcba
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-08-15 17:35:40 +0200

    changed vex theme to wireframe

--- war/assets/js/common/modals/VexModal.js
+++ war/assets/js/common/modals/VexModal.js
@@ -3,6 +3,6 @@ import 'script!../../../../components/Vex/js/vex.combined.min.js';
 export default class VexModal {
 
 	constructor() {
-		vex.defaultOptions.className = 'vex-theme-os';
+		vex.defaultOptions.className = 'vex-theme-wireframe';
 	}
 }
