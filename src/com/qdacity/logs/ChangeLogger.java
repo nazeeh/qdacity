35058d576fedcb7f1fe1a59b83e6a2287e336c9f
Matthias Schoepe <matthias.schoepe@fau.de>
2017-08-18 10:04:36 +0200

    TEST: F4 instance deferred Logging

--- src/com/qdacity/logs/ChangeLogger.java
+++ src/com/qdacity/logs/ChangeLogger.java
@@ -8,7 +8,7 @@ import javax.jdo.PersistenceManager;
 
 public class ChangeLogger {
 
-    private final static boolean DEFERRED = false; //TODO Test//Deferred Change Logging performs better on basic instance
+    private final static boolean DEFERRED = true; //TODO Test//Deferred Change Logging performs better on basic instance
 
     private static PersistenceManager getPersistenceManager() {
 	return PMF.get().getPersistenceManager();
