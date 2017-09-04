1d70a5099a6ad84ef8f8b2d385c885280c702319
Felix Loos <loos.felix93@gmail.com>
2017-09-04 16:14:36 +0200

    Finished create uml-class toolbar-button

--- war/assets/js/pages/uml-editor/toolbar/ButtonAddClass.jsx
+++ war/assets/js/pages/uml-editor/toolbar/ButtonAddClass.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Prompt from '../../../common/modals/Prompt';
+
 import {
 	BtnDefault
 } from '../../../common/styles/Btn.jsx';
@@ -15,10 +17,16 @@ export default class ButtonAddClass extends React.Component {
 	}
 
 	buttonClicked() {
-	    let codeName = 'name';
-	    let mmElementIDs = [1234];
-	    
-	    this.props.createCode(codeName, mmElementIDs);
+		const _this = this;
+
+		let prompt = new Prompt('Give your code a name', 'Code Name');
+
+		prompt.showModal().then(function (codeName) {
+			let mmElementIDs = [];
+			mmElementIDs.push(_this.umlEditor.getMetaModelEntityByName(_this.umlEditor.getMetaModelMapper().getDefaultUmlClassMetaModelName()).id);
+
+			_this.props.createCode(codeName, mmElementIDs);
+		});
 	}
 
 	render() {
