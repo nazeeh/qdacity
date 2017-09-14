7d9c64cdf2fe4335f665c27d501722a034a65daa
Andreas <andreas.kaufmann@fau.de>
2017-09-14 15:58:48 +0200

    Added test for UserEndpoint.findUsers

--- test/com/qdacity/test/UserEndpoint/UserEndpointTest.java
+++ test/com/qdacity/test/UserEndpoint/UserEndpointTest.java
@@ -3,6 +3,9 @@ import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -34,7 +37,7 @@ public class UserEndpointTest {
 
 	@Test
 	public void testUserInsert() {
-		UserEndpointTestHelper.addUser("123456", "asd@asd.de");
+		UserEndpointTestHelper.addUser("firstName","lastName", "123456", "asd@asd.de");
 		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
 		assertEquals(1, ds.prepare(new Query("User")).countEntities(withLimit(10)));
 	}
@@ -43,16 +46,33 @@ public class UserEndpointTest {
 	public void testUserDelete() {
 		com.google.appengine.api.users.User loggedInUser = new com.google.appengine.api.users.User("asd@asd.de", "bla", "123456");
 		
-		UserEndpointTestHelper.addUser("123456", "asd@asd.de");
+		UserEndpointTestHelper.addUser("firstName","lastName", "123456", "asd@asd.de");
 
 		UserEndpoint ue = new UserEndpoint();
 		try {
 			ue.removeUser("123456", loggedInUser);
 		} catch (UnauthorizedException e) {
 			e.printStackTrace();
-			fail("User could not be deleted");
+			fail("User could not be authorized");
 		}
 		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
 		assertEquals(0, ds.prepare(new Query("User")).countEntities(withLimit(10)));
+	}
+	
+	@Test
+	public void testFindUser() {
+		com.google.appengine.api.users.User loggedInUser = new com.google.appengine.api.users.User("asd@asd.de", "bla", "123456");
+		List<User> test = new ArrayList<User>();
+		UserEndpointTestHelper.addUser("firstName","lastName", "123456", "asd@asd.de");
+
+		UserEndpoint ue = new UserEndpoint();
+		try {
+			test = ue.findUsers(null, null, "asd@asd.de", loggedInUser);
+		} catch (UnauthorizedException e) {
+			e.printStackTrace();
+			fail("User could not be authorized");
+		}
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		assertEquals(1, test.size());
 	}
 }
