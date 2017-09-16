480486e7676051018ced84044209f6d7b8636391
Matthias Schoepe <matthias.schoepe@fau.de>
2017-09-16 18:13:14 +0200

    Reverting changes from merge

--- src/com/qdacity/Constants.java
+++ src/com/qdacity/Constants.java
@@ -1,7 +1,7 @@
 package com.qdacity;
 
 public class Constants {
-	public static final String WEB_CLIENT_ID = "309419937441-6d41vclqvedjptnel95i2hs4hu75u4v7.apps.googleusercontent.com";
+	public static final String WEB_CLIENT_ID = "98628102717-19mnq0ntljj8gptvs3fh5v51t5dqj32j.apps.googleusercontent.com";
  	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
-	public static final String VERSION = "v8";
+	public static final String VERSION = "v6";
 }
