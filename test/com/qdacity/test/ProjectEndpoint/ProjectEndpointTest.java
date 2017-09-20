51209792c7e4d6f098bedb1d62d6ec9eaddca94b
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-09-20 11:11:16 +0200

    Added ProjectEndpointTest.java and ProjectEndpointTestHelper.java

---
+++ test/com/qdacity/test/ProjectEndpoint/ProjectEndpointTest.java
@@ -0,0 +1,22 @@
+package com.qdacity.test.ProjectEndpoint;
+import org.junit.After;
+import org.junit.Before;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
+public class ProjectEndpointTest {
+
+	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+	private final com.google.appengine.api.users.User testUser = new com.google.appengine.api.users.User("asd@asd.de", "bla", "123456");
+	@Before
+	public void setUp() {
+		helper.setUp();
+	}
+
+	@After
+	public void tearDown() {
+		helper.tearDown();
+	}
+	
+}
---
+++ test/com/qdacity/test/ProjectEndpoint/ProjectEndpointTestHelper.java
@@ -0,0 +1,5 @@
+package com.qdacity.test.ProjectEndpoint;
+
+public class ProjectEndpointTestHelper {
+
+}
