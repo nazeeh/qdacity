a2f1b27e2f89d62b362eadfe3e4a646add479258
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-09-20 11:23:48 +0200

    Added test for inserting projects

--- test/com/qdacity/test/ProjectEndpoint/ProjectEndpointTest.java
+++ test/com/qdacity/test/ProjectEndpoint/ProjectEndpointTest.java
@@ -1,9 +1,20 @@
 package com.qdacity.test.ProjectEndpoint;
+
+import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
+import com.google.api.server.spi.response.UnauthorizedException;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Query;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.qdacity.test.UserEndpoint.UserEndpointTestHelper;
 
 public class ProjectEndpointTest {
 
@@ -18,5 +29,19 @@ public class ProjectEndpointTest {
 	public void tearDown() {
 		helper.tearDown();
 	}
-	
+
+	@Test
+	public void testProjectInsert() {
+		UserEndpointTestHelper.addUser("123456", "asd@asd.de", "firstName", "lastName");
+		com.google.appengine.api.users.User loggedInUser = new com.google.appengine.api.users.User("asd@asd.de", "bla", "123456");
+		try {
+			ProjectEndpointTestHelper.addProject(1L, "New Project", "A description", loggedInUser);
+		} catch (UnauthorizedException e) {
+			e.printStackTrace();
+			fail("User could not be authorized");
+		}
+
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		assertEquals(1, ds.prepare(new Query("Project")).countEntities(withLimit(10)));
+	}
 }
--- test/com/qdacity/test/ProjectEndpoint/ProjectEndpointTestHelper.java
+++ test/com/qdacity/test/ProjectEndpoint/ProjectEndpointTestHelper.java
@@ -1,5 +1,18 @@
 package com.qdacity.test.ProjectEndpoint;
 
-public class ProjectEndpointTestHelper {
+import com.google.api.server.spi.response.UnauthorizedException;
+import com.qdacity.endpoint.ProjectEndpoint;
+import com.qdacity.project.Project;
 
+public class ProjectEndpointTestHelper {
+	static public void addProject(Long id, String name, String description, com.google.appengine.api.users.User loggedInUser) throws UnauthorizedException {
+		Project project = new Project();
+		project.setId(id);
+		project.setName(name);
+		project.setDescription(description);
+
+
+		ProjectEndpoint ue = new ProjectEndpoint();
+		ue.insertProject(project, loggedInUser);
+	}
 }
