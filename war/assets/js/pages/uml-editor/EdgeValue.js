57260ec5d631a96ff6b0b369decc62d000049b43
Felix Loos <loos.felix93@gmail.com>
2017-09-25 21:03:30 +0200

    Added EdgeValue which stores information for edges

---
+++ war/assets/js/pages/uml-editor/EdgeValue.js
@@ -0,0 +1,14 @@
+export default class EdgeValue {
+
+	constructor(relationId) {
+		this.relationId = relationId;
+	}
+
+	getRelationId() {
+		return this.relationId;
+	}
+
+	setRelationId(relationId) {
+		this.relationId = relationId;
+	}
+}
