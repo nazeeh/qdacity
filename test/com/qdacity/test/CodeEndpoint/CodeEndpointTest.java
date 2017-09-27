2e71083de17a291d4dc7cafd1647da9e2eee0385
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-09-27 15:10:50 +0200

    Extended test for code relocation

--- test/com/qdacity/test/CodeEndpoint/CodeEndpointTest.java
+++ test/com/qdacity/test/CodeEndpoint/CodeEndpointTest.java
@@ -18,6 +18,7 @@ import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestC
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.appengine.tools.development.testing.LocalTaskQueueTestConfig;
 import com.qdacity.PMF;
+import com.qdacity.endpoint.CodeEndpoint;
 import com.qdacity.project.codesystem.Code;
 import com.qdacity.test.CodeSystemEndpoint.CodeSystemTestHelper;
 import com.qdacity.test.ProjectEndpoint.ProjectEndpointTestHelper;
@@ -55,7 +56,7 @@ public class CodeEndpointTest {
 			e.printStackTrace();
 		}
 
-		CodeEndpointTestHelper.addCode(2L, 2L, 1L, 1L, "authorName", "fff", testUser);
+		CodeEndpointTestHelper.addCode(22L, 2L, 1L, 1L, "authorName", "fff", testUser);
 
 		assertEquals(2, ds.prepare(new Query("Code")).countEntities(withLimit(10))); // it is two because of the codesystem
 	}
@@ -87,10 +88,10 @@ public class CodeEndpointTest {
 		PersistenceManager mgr = getPersistenceManager();
 		mgr.setIgnoreCache(true);
 		try {
-			Code code = mgr.getObjectById(Code.class, 2L);
+			Code code = mgr.getObjectById(Code.class, 22L);
 			code.setColor("f0f");
 			CodeEndpointTestHelper.updateCode(code, testUser);
-			code = mgr.getObjectById(Code.class, 2L);
+			code = mgr.getObjectById(Code.class, 22L);
 			assertEquals("f0f", code.getColor());
 		} finally {
 			mgr.close();
@@ -101,9 +102,12 @@ public class CodeEndpointTest {
 
 	/**
 	 * Tests if a registered user can relocate the properties of a code in a project he is authorized for
+	 * 
+	 * @throws UnauthorizedException
 	 */
 	@Test
-	public void testCodeRelocate() {
+	public void testCodeRelocate() throws UnauthorizedException {
+		CodeEndpoint ce = new CodeEndpoint();
 		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
 		testCodeInsert();
 		CodeEndpointTestHelper.addCode(33L, 3L, 1L, 1L, "authorName", "fff", testUser);
@@ -112,12 +116,28 @@ public class CodeEndpointTest {
 
 		PersistenceManager mgr = getPersistenceManager();
 		mgr.setIgnoreCache(true);
+		mgr.setMultithreaded(true);
 		try {
+			Code code = mgr.getObjectById(Code.class, 22L);
+			assertEquals(new Long(1), code.getParentID());
+
+			code = ce.getCode(44L, testUser);
+			assertEquals(new Long(1), code.getParentID());
 			CodeEndpointTestHelper.relocateCode(33L, 2L, testUser);
 			CodeEndpointTestHelper.relocateCode(44L, 2L, testUser);
 
-			Code code = mgr.getObjectById(Code.class, 2L);
-			assertEquals(2, code.getSubCodesIDs().size());
+			code = mgr.getObjectById(Code.class, 33L);
+			assertEquals(new Long(2), code.getParentID());
+
+			code = ce.getCode(44L, testUser);
+			assertEquals(new Long(2), code.getParentID());
+
+			CodeEndpointTestHelper.relocateCode(44L, 3L, testUser);
+			CodeEndpointTestHelper.relocateCode(44L, 3L, testUser);
+
+			code = ce.getCode(44L, testUser);
+			assertEquals(new Long(3), code.getParentID());
+
 		} finally {
 			mgr.close();
 		}
