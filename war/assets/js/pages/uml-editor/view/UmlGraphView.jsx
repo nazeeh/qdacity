23d12a05c7af3c05330cadcc1987fca47776cc50
Felix Loos <loos.felix93@gmail.com>
2017-09-29 10:24:48 +0200

    Moved graph view edge-connection logic into a new component

--- war/assets/js/pages/uml-editor/view/UmlGraphView.jsx
+++ war/assets/js/pages/uml-editor/view/UmlGraphView.jsx
@@ -7,6 +7,7 @@ import CellValue from './CellValue.js';
 import EdgeValue from './EdgeValue.js';
 
 import GraphStyles from './GraphStyles.js';
+import GraphConnectionHandler from './GraphConnectionHandler.js';
 
 import {
 	EdgeType
@@ -41,23 +42,16 @@ export default class UmlGraphView extends React.Component {
 		this.maxZoomPercentage = 150;
 
 		this.umlGraphContainer = null;
+
+		this.graphConnectionHandler = null;
 		this.hoverButtons = null;
 
 		this.graph = null;
 		this.layout = null;
 
-		this.cellMarker = null;
-		this.connectionHandler = null;
-
-		this.connectionEdgeStartCell = null;
-
 		this.panning = false;
 
 		this.lastSelectedCells = null;
-	}
-
-	isConnectingEdge() {
-		return this.connectionEdgeStartCell != null;
 	}
 
 	componentDidMount() {
@@ -156,74 +150,7 @@ export default class UmlGraphView extends React.Component {
 	}
 
 	initializeConnections() {
-		const _this = this;
-
-		this.connectionEdgeStartCell = null;
-
-		// Cell Marker
-		this.cellMarker = new mxCellMarker(this.graph);
-
-		// Mouse listener
-		const connectionMouseListener = {
-			mouseDown: function (sender, me) {},
-			mouseMove: function (sender, me) {},
-			mouseUp: function (sender, me) {
-				if (_this.isConnectingEdge()) {
-					me.consumed = false;
-					_this.cellMarker.process(me);
-				}
-			}
-		};
-
-		_this.graph.addMouseListener(connectionMouseListener);
-
-		// Connection Handler
-		this.connectionHandler = new mxConnectionHandler(this.graph, function (source, target, style) {
-			const edgeValue = new EdgeValue(-1);
-
-			const edge = new mxCell(edgeValue, new mxGeometry());
-			edge.setEdge(true);
-			edge.setStyle(style);
-			edge.geometry.relative = true;
-			return edge;
-		});
-		this.connectionHandler.livePreview = true;
-		this.connectionHandler.select = false;
-
-		this.connectionHandler.isValidTarget = function (cell) {
-			return _this.isCellUmlClass(cell);
-		};
-
-		this.connectionHandler.getEdgeWidth = function (valid) {
-			return 1;
-		};
-
-		this.connectionHandler.addListener(mxEvent.START, function (sender, evt) {
-			_this.connectionEdgeStartCell = evt.properties.state.cell;
-		});
-
-		this.connectionHandler.addListener(mxEvent.RESET, function (sender, evt) {
-			_this.selectCell(_this.connectionEdgeStartCell);
-
-			_this.connectionEdgeStartCell = null;
-		});
-
-		this.connectionHandler.addListener(mxEvent.CONNECT, function (sender, evt) {
-			_this.cellMarker.process(new mxMouseEvent());
-
-			_this.connectionEdgeStartCell = null;
-
-			const edgeType = evt.properties.cell.style;
-			const sourceNode = evt.properties.cell.source;
-			const destinationNode = evt.properties.cell.target;
-
-			const sourceCode = _this.props.umlEditor.getCodeById(sourceNode.value.getCodeId());
-			const destinationCode = _this.props.umlEditor.getCodeById(destinationNode.value.getCodeId());
-
-			_this.props.umlEditor.createEdge(sourceCode, destinationCode, edgeType);
-
-			_this.selectCell(sourceNode);
-		});
+		this.graphConnectionHandler = new GraphConnectionHandler(this.props.umlEditor, this, this.graph);
 	}
 
 	initializePanning() {
@@ -531,6 +458,10 @@ export default class UmlGraphView extends React.Component {
 		}
 	}
 
+	hideHoverButtons() {
+		this.hoverButtons.hide();
+	}
+
 	toggleCollapseCell(cell) {
 		cell.setCollapsed(!cell.isCollapsed());
 		this.recalculateNodeSize(cell);
@@ -562,20 +493,19 @@ export default class UmlGraphView extends React.Component {
 		this.recalculateNodeSize(cell);
 	}
 
+	// TODO
+	isConnectingEdge() {
+		return this.graphConnectionHandler.isConnectingEdge();
+	}
+
 	startConnecting(edgeType) {
-		let edge = this.graph.createEdge(null, null, null, null, null, edgeType);
-		let edgeState = new mxCellState(this.graph.view, edge, this.graph.getCellStyle(edge));
-
-		let cellState = this.graph.getView().getState(this.graph.getSelectionCell(), true);
-
-		this.connectionHandler.start(cellState, 0, 0, edgeState);
-
-		this.hoverButtons.hide();
+		this.graphConnectionHandler.startConnecting(edgeType);
 	}
 
 	resetConnectingEdge() {
-		this.connectionHandler.reset();
+		this.graphConnectionHandler.resetConnectingEdge();
 	}
+	// TODO
 
 	clearSelection() {
 		this.graph.clearSelection();
