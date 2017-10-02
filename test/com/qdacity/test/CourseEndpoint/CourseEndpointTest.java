9a22fc95c7e45b1b51a4c715df6edb2f11a7c94d
nazeeh <nazeehammari@Nazeehs-MacBook-Pro.local>
2017-10-02 15:23:47 +0200

    Added a unit test for removeCourseAuthorization

--- test/com/qdacity/test/CourseEndpoint/CourseEndpointTest.java
+++ test/com/qdacity/test/CourseEndpoint/CourseEndpointTest.java
@@ -1,13 +1,16 @@
 package com.qdacity.test.CourseEndpoint;
 
 import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.datastore.DatastoreService;
@@ -75,4 +78,45 @@ public class CourseEndpointTest {
 
 		assertEquals(0, ds.prepare(new Query("Course")).countEntities(withLimit(10)));
 	}
+	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
+	/**
+	 * Tests if Courses from other users can be not be deleted
+	 * 
+	 * @throws UnauthorizedException
+	 */
+	@Test
+	public void testCourseRemoveAuthorization() throws UnauthorizedException {
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		assertEquals(0, ds.prepare(new Query("User")).countEntities(withLimit(10)));
+		com.google.appengine.api.users.User loggedInUserA = new com.google.appengine.api.users.User("asd@asd.de", "bla", "1");
+		UserEndpointTestHelper.addUser("asd@asd.de", "User", "A", loggedInUserA);
+		assertEquals(1, ds.prepare(new Query("User")).countEntities(withLimit(10)));
+		com.google.appengine.api.users.User loggedInUserB = new com.google.appengine.api.users.User("asd@asd.de", "bla", "2");
+		UserEndpointTestHelper.addUser("asd@asd.de", "User", "B", loggedInUserB);
+		assertEquals(2, ds.prepare(new Query("User")).countEntities(withLimit(10)));
+
+		try {
+			CourseEndpointTestHelper.addCourse(1L, "New Course", "A description", loggedInUserA);
+		} catch (UnauthorizedException e) {
+			e.printStackTrace();
+			fail("User could not be authorized for course creation");
+		}
+
+		assertEquals(1, ds.prepare(new Query("Course")).countEntities(withLimit(10)));
+
+		expectedException.expect(UnauthorizedException.class);
+		expectedException.expectMessage(is("User is Not Authorized"));
+
+		CourseEndpointTestHelper.removeCourse(1L, loggedInUserB); // User B should not be able to delete course from user A
+
+		// The course added by User A should still exist
+		assertEquals(1, ds.prepare(new Query("Course")).countEntities(withLimit(10)));
+	}
+	
+	
+	
+	
 }
