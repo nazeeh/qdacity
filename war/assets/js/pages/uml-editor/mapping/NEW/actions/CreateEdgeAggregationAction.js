3dfc4666c8f00b72337965f5517cf8547de249cb
Felix Loos <loos.felix93@gmail.com>
2017-10-02 12:27:38 +0200

    Implemented Aggregation + DirectedAssociation EdgeAction

---
+++ war/assets/js/pages/uml-editor/mapping/NEW/actions/CreateEdgeAggregationAction.js
@@ -0,0 +1,12 @@
+import CreateEdgeAction from './CreateEdgeAction.js';
+
+import {
+	EdgeType
+} from '../../../util/EdgeType.js';
+
+export default class CreateEdgeAggregationAction extends CreateEdgeAction {
+	
+	getEdgeType() {
+		return EdgeType.AGGREGATION;
+	}
+}
