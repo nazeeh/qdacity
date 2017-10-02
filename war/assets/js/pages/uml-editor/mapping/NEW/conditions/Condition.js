3d41ddb4ae411d4a4743c308f27592b6cb09576d
Felix Loos <loos.felix93@gmail.com>
2017-10-02 11:43:15 +0200

    Added a Condition class that represents a condition for the metamodel mapping

---
+++ war/assets/js/pages/uml-editor/mapping/NEW/conditions/Condition.js
@@ -0,0 +1,46 @@
+import AndCondition from './AndCondition.js';
+import OrCondition from './OrCondition.js';
+import HasMetaModelEntityCondition from './HasMetaModelEntityCondition.js';
+
+export default class Condition {
+	
+	static and() {		
+		if (arguments == null || arguments.length < 2) {
+			throw new Error('AND condition requires at least 2 sub-conditions');
+		}
+		
+		return new AndCondition(arguments);
+	}
+	
+	static or() {	
+		if (arguments == null || arguments.length < 2) {
+			throw new Error('OR condition requires at least 2 sub-conditions');
+		}
+		
+		return new OrCondition(arguments);		
+	}
+	
+	static isValidNode(evaluationTarget) {
+		// TODO
+	}
+	
+	static hasMetaModelEntity(metaModelEntityName, evaluationTarget) {
+		return new HasMetaModelEntityCondition(name, target);
+	}
+	
+	constructor() {
+		this.rule = null;
+	}
+	
+	getRule() {
+		return this.rule;
+	}
+	
+	setRule(rule) {
+		this.rule = rule;
+	}
+	
+	evaluate(target) {
+		return false;
+	}
+}
