cee74f8f4f31262316b5292128bcd472e7b88c2c
Felix Loos <loos.felix93@gmail.com>
2017-10-02 11:43:36 +0200

    Implemented Add and Or condition

---
+++ war/assets/js/pages/uml-editor/mapping/NEW/conditions/AndCondition.js
@@ -0,0 +1,12 @@
+import MultipleCondition from './MultipleCondition.js';
+
+export default class AndCondition extends MultipleCondition {
+	
+	constructor(conditions) {
+		super(conditions);
+	}
+
+	compare(resultA, resultB) {
+		return resultA && resultB;
+	}
+}
---
+++ war/assets/js/pages/uml-editor/mapping/NEW/conditions/MultipleCondition.js
@@ -0,0 +1,40 @@
+import Condition from './Condition.js';
+
+export default class MultipleCondition extends Condition {
+	
+	constructor(conditions) {
+		this.conditions = conditions;		
+	}
+	
+	setRule(rule) {
+		super.setRule(rule);
+
+		// Set rule for children
+		if (conditions != null && conditions.length > 0) {
+			for (let i = 0; i < this.conditions.length; i++) {
+				this.conditions[i].setRule(rule);
+			}
+		}
+	}
+	
+	evaluate(target) {
+		let result = true;
+		
+		if (conditions != null && conditions.length > 0) {
+			result = conditions[0].execute(target);
+			
+			for (let i = 1; i < this.conditions.length; i++) {
+				result = this.compare(result, conditions[i].execute(target));
+			}
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Override this method.
+	 */
+	compare(resultA, resultB) {
+		return null;
+	}
+}
