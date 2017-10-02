0ab894dea68133d5040e498f8f4f97d8ed504dd5
Felix Loos <loos.felix93@gmail.com>
2017-10-02 12:49:52 +0200

    Implemented static create methods for Action

--- war/assets/js/pages/uml-editor/mapping/NEW/actions/Action.js
+++ war/assets/js/pages/uml-editor/mapping/NEW/actions/Action.js
@@ -1,9 +1,34 @@
 import CreateNodeAction from './CreateNodeAction.js';
+import CreateEdgeGeneralizationAction from './CreateEdgeGeneralizationAction.js';
+import CreateEdgeAggregationAction from './CreateEdgeAggregationAction.js';
+import CreateEdgeDirectedAssociationAction from './CreateEdgeDirectedAssociationAction.js';
+import CreateClassFieldAction from './CreateClassFieldAction.js';
+import CreateClassMethodAction from './CreateClassMethodAction.js';
 
 export default class Action {
 
 	static createNode() {		
 		return new CreateNodeAction();
+	}
+	
+	static createGeneralizationAction() {
+		return new CreateEdgeGeneralizationAction();		
+	}
+	
+	static createAggregationAction() {
+		return new CreateEdgeAggregationAction();	
+	}
+	
+	static createDirectedAssociationAction() {
+		return new CreateEdgeDirectedAssociationAction();	
+	}
+	
+	static createClassFieldAction() {
+		return new CreateClassFieldAction();	
+	}
+	
+	static createClassMethodAction() {
+		return new CreateClassMethodAction();	
 	}
 
 	constructor() {
