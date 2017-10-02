0cee40edb243306a9c960be1e59198d231baa3d9
Felix Loos <loos.felix93@gmail.com>
2017-10-02 12:11:40 +0200

    Implemented NodeAction

--- war/assets/js/pages/uml-editor/mapping/NEW/actions/CreateNodeAction.js
+++ war/assets/js/pages/uml-editor/mapping/NEW/actions/CreateNodeAction.js
@@ -1,8 +1,18 @@
 import Action from './Action.js';
 
+import {
+	Target
+} from '../Target.js';
+
 export default class CreateNodeAction extends Action {
 	
 	execute(target) {
-		
+		if (this.getRule().getTargetType() == Target.CODE) {
+			const code = target;
+			this.getRule().getMapper().getUmlEditor().addNode(code);
+		}
+		else {
+			throw new Error('Cant create node for target type ' + this.getRule().getTargetType());
+		}
 	}
 }
