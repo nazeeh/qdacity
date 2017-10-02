b121e5aaeb4a8887e5ffd720a38cbeeb118ccbbb
Felix Loos <loos.felix93@gmail.com>
2017-10-02 11:44:00 +0200

    Implemented HasMetaModelEntity Condition

---
+++ war/assets/js/pages/uml-editor/mapping/NEW/conditions/EvaluationTarget.js
@@ -0,0 +1,5 @@
+export const EvaluationTarget = {
+	THIS: 1,
+	SOURCE: 2,
+	DESTINATION: 3
+};
---
+++ war/assets/js/pages/uml-editor/mapping/NEW/conditions/HasMetaModelEntityCondition.js
@@ -0,0 +1,76 @@
+import Condition from './Condition.js';
+
+import {
+	Target
+} from '../Target.js';
+
+import {
+	EvaluationTarget
+} from './EvaluationTarget.js';
+
+export default class HasMetaModelEntityCondition extends Condition {
+
+	constructor(metaModelEntityName, evaluationTarget) {
+		this.metaModelEntityName = metaModelEntityName;
+		this.evaluationTarget = evaluationTarget;
+	}
+
+	evaluate(target) {
+		if (this.getRule().getTargetType() == Target.CODE) {
+			return this.evaluateCode(target);
+		}
+		else if (this.getRule.getTargetType() == Target.RELATION) {
+			return this.evaluateRelation(target);
+		}
+		else {
+			throw new Error('Unknown target type ' + this.getRule.getTarget());
+		}
+	}
+	
+	evaluateCode(code) {
+		let mmElementIds = code.mmElementIDs;
+		
+		if (mmElementIds != null) {
+			for (let i = 0; i < mmElementIds.length; i++) {
+				const mmElementId = mmElementIds[i];
+
+				const entity = this.getMetaModelEntityById(mmElementId);
+				
+				if (entity != null && entity.name == this.metaModelEntityName) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	} 
+	
+	evaluateRelation(relation) {
+		// Relation has metaModelEntity
+		if (this.evaluationTarget == null || this.evaluationTarget == EvaluationTarget.THIS) {
+			let mmElementId = relation.mmElementId;
+	
+			const entity = this.getMetaModelEntityById(mmElementId);
+			
+			return entity != null && entity.name == this.metaModelEntityName;		
+		}
+		// Relation source code has metaModelEntity
+		else if (this.evaluationTarget == EvaluationTarget.SOURCE) {
+			let sourceCode = this.getRule().getMapper().getCodeById(relation.key.parent.id);
+			return this.evaluateCode(sourceCode);
+		}
+		// Relation destination code has metaModelEntity
+		else if (this.evaluationTarget == EvaluationTarget.DESTINATION) {
+			let destinationCode = this.getRule().getMapper().getCodeByCodeId(relation.codeId);
+			return this.evaluateCode(destinationCode);
+		}
+		// Error
+		else {
+			throw new Error('Unknown target type ' + this.evaluationTarget);
+		}
+	}
+	
+	getMetaModelEntityById(metaModelElementId) {
+		return this.getRule().getMapper().getMetaModelEntities().find((element) => element.id == metaModelElementId);;
+	}
+}
