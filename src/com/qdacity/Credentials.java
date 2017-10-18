23f7afcddab9de777721cc8298d18483ede7a002
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-10-18 15:40:43 +0200

    Update Credentials.java

--- src/com/qdacity/Credentials.java
+++ src/com/qdacity/Credentials.java
@@ -1,6 +1,6 @@
 package com.qdacity;
 
 public class Credentials {
-	public static final String SENDGRID_PW = "";
-	public static final String SENDGRID_USER = "";
+	public static final String SENDGRID_PW = "tIBEesvIIV5hUbKQNCwa";
+	public static final String SENDGRID_USER = "unieins";
 }
