74ac2ac387e707af16c26c138326ec8e3b368434
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-11-05 19:36:10 +0100

    Add Administration link to NavBar.jsx

--- war/assets/js/common/NavBar.jsx
+++ war/assets/js/common/NavBar.jsx
@@ -85,6 +85,7 @@ export default class NavBar extends React.Component {
 											</li>
 										</ul>
 								</StyledSigninTab>
+								<li><StyledNavbarItem href="/Admin">Administration</StyledNavbarItem></li>
 							</ul>
 						</div>
 					</div>
