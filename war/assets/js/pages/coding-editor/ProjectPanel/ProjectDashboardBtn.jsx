4566ecb9d664f02a269c29bcb8785df159f8ac1e
Andreas Kaufmann <andreas.kaufmann@fau.de>
2017-11-09 14:22:31 +0100

    Added component, missing from last commit

---
+++ war/assets/js/pages/coding-editor/ProjectPanel/ProjectDashboardBtn.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components';
+import {
+	BtnDefault
+} from '../../../common/styles/Btn.jsx';
+
+const StyledPrjDasboardBtn = BtnDefault.extend `
+	text-align: center;
+	width: 100%;
+	margin-bottom:5px;
+`;
+
+export default class ProjectDashboardBtn extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	projectDashboardBtnClick(prj) {
+		this.props.history.push('/ProjectDashboard?project=' + prj.id + '&type=' + prj.type);
+	}
+
+	render() {
+		return (
+			<StyledPrjDasboardBtn  onClick={() => {this.projectDashboardBtnClick(this.props.project);}}>
+				<i className="fa fa-home fa-lg"></i>
+				<span>Project Dashboard</span>
+			</StyledPrjDasboardBtn>
+		);
+	}
+}
