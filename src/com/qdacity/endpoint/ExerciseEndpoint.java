c9eab3ed72df3f17816a01c6de5e52f7879b5f34
nazeeh <nazeehammari@Nazeehs-MacBook-Pro.local>
2017-11-13 19:54:59 +0100

    Created ExerciseEndpoint.java

---
+++ src/com/qdacity/endpoint/ExerciseEndpoint.java
@@ -0,0 +1,17 @@
+package com.qdacity.endpoint;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.qdacity.Constants;
+
+
+@Api(name = "qdacity",
+	version = Constants.VERSION,
+	namespace = @ApiNamespace(ownerDomain = "qdacity.com",
+		ownerName = "qdacity.com",
+		packagePath = "server.exercise"))
+public class ExerciseEndpoint {
+
+	
+
+}
