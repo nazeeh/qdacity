7fb1301de8a4ceeffe37fc998d2aa87b917227b0
nazeeh <nazeehammari@Nazeehs-MacBook-Pro.local>
2017-11-13 19:52:44 +0100

    Created the object Exercise

---
+++ src/com/qdacity/exercise/Exercise.java
@@ -0,0 +1,51 @@
+package com.qdacity.exercise;
+
+import java.io.Serializable;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(
+	identityType = IdentityType.APPLICATION)
+
+
+public class Exercise implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5474843555409444467L;
+
+	@PrimaryKey
+	@Persistent(
+		valueStrategy = IdGeneratorStrategy.IDENTITY)
+	Long id;
+	
+	@Persistent
+	Long termCourseID;
+
+	public Exercise() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getTermCourseID() {
+		return termCourseID;
+	}
+	
+	public void setTermCourseID(Long termCourseID) {
+		this.termCourseID = termCourseID;
+	}
+	
+}
--- war/assets/js/pages/termCourse-dashboard/TermCourse.js
+++ war/assets/js/pages/termCourse-dashboard/TermCourse.js
@@ -20,5 +20,5 @@ export default class TermCourse {
 	getParticipants () {
 		return this.participants;
 	}
-	
+
 }
