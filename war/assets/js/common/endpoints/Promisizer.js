ddbda12ee358c9f0b29ec0046b8e4b5d34b6689b
Georg <georg.schwarz@fau.de>
2017-11-15 16:46:04 +0100

    added ensureTokenAvailability into Promisizer.js in order to deal with token loss by page reload

--- war/assets/js/common/endpoints/Promisizer.js
+++ war/assets/js/common/endpoints/Promisizer.js
@@ -1,19 +1,35 @@
+import firebaseWrapper from '../firebase';
+
 export default class Promisizer {
 	constructor() {}
 
 	static makePromise(apiMethod) {
 		var promise = new Promise(
 			function (resolve, reject) {
-				apiMethod.execute(function (resp) {
-					if (!resp.code) {
-						resolve(resp);
-					} else {
-						console.log(resp.code + " : " + resp.message);
-						reject(resp);
-					}
-				});
+        Promisizer.ensureTokenAvailability().then(function() {
+					apiMethod.execute(function (resp) {
+						if (!resp.code) {
+							resolve(resp);
+						} else {
+							console.log(resp.code + " : " + resp.message);
+							reject(resp);
+						}
+					});
+        }, function() {
+          console.log('Could not make a promise because the token was not available!');
+        })
 			}
 		);
+
 		return promise;
+	}
+
+  /**
+	 * Ensures that an authentication token is placed in gapi.
+	 * This call is required in order to load the token after site refreshes.
+   * @return {Promise}
+   */
+	static ensureTokenAvailability() {
+    return firebaseWrapper.synchronizeTokenWithGapi();
 	}
 }
