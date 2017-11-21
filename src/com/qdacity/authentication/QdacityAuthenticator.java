6d95f3677ba7e909133f7adf093b29dda9841c10
Georg <georg.schwarz@fau.de>
2017-11-21 14:03:47 +0100

    added first implementation of google token validation and added compile time dependencies (runtime deps still missing)

--- src/com/qdacity/authentication/QdacityAuthenticator.java
+++ src/com/qdacity/authentication/QdacityAuthenticator.java
@@ -1,9 +1,20 @@
 package com.qdacity.authentication;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.logging.Level;
+
 import javax.servlet.http.HttpServletRequest;
 
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.server.spi.auth.common.User;
 import com.google.api.server.spi.config.Authenticator;
+import com.qdacity.Constants;
 
 /**
  * Custom authentication class that interacts with google cloud api and injects automatically a User object.
@@ -17,6 +28,13 @@ import com.google.api.server.spi.config.Authenticator;
  */
 public class QdacityAuthenticator implements Authenticator {
 	
+	private static final JacksonFactory jacksonFactory = new JacksonFactory();
+	
+	GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(UrlFetchTransport.getDefaultInstance(), jacksonFactory)
+            .setAudience(Arrays.asList(Constants.WEB_CLIENT_ID))
+            .setIssuer("https://accounts.google.com")
+            .build();
+	
 
     /**
      * Validates the received token and returns a User object if the token was valid.
@@ -29,8 +47,24 @@ public class QdacityAuthenticator implements Authenticator {
 
         //verify
         if(authorizationHeader != null) {
-        	String idToken = authorizationHeader.replace("Bearer ", "");
+        	String idTokenString = authorizationHeader.replace("Bearer ", "");
         	
+    		GoogleIdToken idToken = null;
+			try {
+				idToken = verifier.verify(idTokenString);
+			} catch (GeneralSecurityException | IOException e) {
+				java.util.logging.Logger.getLogger("logger").log(Level.WARNING, "The given authentication token could not be verified.");
+				return null;
+			}
+    		if (idToken != null) {
+    			Payload payload = idToken.getPayload();
+    			String userId = payload.getSubject();
+    			String email = payload.getEmail();
+    		  
+    			return new User(userId, email);
+    		} else {
+    			java.util.logging.Logger.getLogger("logger").log(Level.WARNING, "Invalid ID token.");
+    		}
         }
 
         return null;
