f25a3c14c8379cd1b06933f15dd7f0c9cc004750
Georg <georg.schwarz@fau.de>
2017-11-22 08:16:55 +0100

    removed ensurement of token availability in the Promizizer because now the constructor of Account.jsx makes sure that the token is updated

--- war/assets/js/common/endpoints/Promisizer.js
+++ war/assets/js/common/endpoints/Promisizer.js
@@ -9,31 +9,17 @@ export default class Promisizer {
 	static makePromise(apiMethod) {
 		var promise = new Promise(
 			function (resolve, reject) {
-        Promisizer.ensureTokenAvailability().then(function() {
-					apiMethod.execute(function (resp) {
-						if (!resp.code) {
-							resolve(resp);
-						} else {
-							console.log(resp.code + " : " + resp.message);
-							reject(resp);
-						}
-					});
-        }, function() {
-        	console.log('Could not make a promise because the token was not available!');
-        })
+				apiMethod.execute(function (resp) {
+					if (!resp.code) {
+						resolve(resp);
+					} else {
+						console.log(resp.code + " : " + resp.message);
+						reject(resp);
+					}
+				});
 			}
 		);
 
 		return promise;
-	}
-
-	/**
- 	* Ensures that an authentication token is placed in gapi.
-	* This call is required in order to load the token after site refreshes.
-	* Loads data from cache, so this means not a request each time.
-	* @return {Promise}
-   	*/
-	static ensureTokenAvailability() {
-    	return authenticationProvider.synchronizeTokenWithGapi();
 	}
 }
