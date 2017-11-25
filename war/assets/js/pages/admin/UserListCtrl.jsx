310748d636d868b9bfe1fc10d4073d35442cac21
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-11-25 18:10:01 +0100

    Center edit and delete buttons

--- war/assets/js/pages/admin/UserListCtrl.jsx
+++ war/assets/js/pages/admin/UserListCtrl.jsx
@@ -12,21 +12,17 @@ export default class UserListCtrl extends React.Component {
 			user: this.props.user,
 			test: this.props.test
 		};
-		this.showUserInfo = this.showUserInfo.bind(this);
-		this.removeUser = this.removeUser.bind(this);
-
 	}
 
 	showUserInfo() {
-		var _this = this;
-		var user = _this.props.user;
-		var modal = new CustomForm('Edit User Info');
-		modal.addTextInput('firstName', "First Name", '', user.givenName);
+        const user = this.props.user;
+        const modal = new CustomForm('Edit User Info');
+        modal.addTextInput('firstName', "First Name", '', user.givenName);
 		modal.addTextInput('lastName', "Last Name", '', user.surName);
 		modal.addTextInput('email', "Email", '', user.email);
 		modal.addSelect('type', ["USER", "ADMIN"], "Type", user.type);
-		modal.showModal().then(function (data) {
-			_this.props.updateUser(data)
+		modal.showModal().then((data) => {
+			this.props.updateUser(data)
 		});
 	}
 
@@ -39,18 +35,14 @@ export default class UserListCtrl extends React.Component {
 	}
 
 	render() {
-		var _this = this;
-		var classes = 'btnUpdateDoc ' + styles.block;
 		return (
-			<div className={styles.center}>
-	      	<div className={classes}>
-				<BtnDefault id="btnUserInfo" className="btn btn-default" onClick={_this.showUserInfo.bind(null, null)}>
-					<i className="fa fa-pencil fa-1x"></i>
+			<div className="center">
+				<BtnDefault id="btnUserInfo" onClick={() => this.showUserInfo()}>
+					<i className="fa fa-pencil fa-1x"/>
 				</BtnDefault>
-				<BtnDefault id="btnRemoveUser" className="btn btn-default" href="#" onClick={this.removeUser}>
-					<i className="fa fa-trash fa-1x"></i>
+				<BtnDefault id="btnRemoveUser" href="#" onClick={() => this.removeUser()}>
+					<i className="fa fa-trash fa-1x"/>
 				</BtnDefault>
-			</div>
 		  </div>
 		);
 	}
