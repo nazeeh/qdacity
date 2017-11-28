33bac2ff843e613916ec809b89b8823f98f27bc9
Andreas Mischke <andreas@mischke.me>
2017-11-28 17:53:12 +0100

    Fix deployment config
    - Fixed version number for backend
    - Added version number for rts-svc

--- src/com/qdacity/Constants.java
+++ src/com/qdacity/Constants.java
@@ -3,5 +3,5 @@ package com.qdacity;
 public class Constants {
 	public static final String WEB_CLIENT_ID = "401624009491-sosmr8p6fqa31gsadi9kd6kbq4c27nvl.apps.googleusercontent.com";
  	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
-	public static final String VERSION = "v1";
+	public static final String VERSION = "v10";
 }
