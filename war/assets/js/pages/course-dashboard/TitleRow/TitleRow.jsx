01c16854303dc77d47ea287e2f6aebc501d4e914
Ammari <nazeeh.ammari@siemens.com>
2017-11-30 16:35:15 +0100

    Added the title row with course name to CourseDashboard

---
+++ war/assets/js/pages/course-dashboard/TitleRow/TitleRow.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledName = styled.span `
+	margin-left: 5px;
+`;
+
+const StyledHeading = styled.h5 `
+	margin-left: 5px;
+`;
+
+export default class TitleRow extends React.Component {
+	constructor(props) {
+		super(props);
+
+	}
+
+	render() {
+		var courseName = (this.props.course.name ? this.props.course.name : "")
+		return (
+			<StyledHeading>
+			<i className="fa fa-newspaper-o"></i>
+			<StyledName onClick={(e) => this.courseTitleClicked(e)}>
+				This course: {courseName}
+			</StyledName>
+          </StyledHeading>
+		);
+	}
+
+
+}
