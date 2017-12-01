8e67c2066103cdecd39aa2e69c129ec164622539
Georg <georg.schwarz@fau.de>
2017-12-01 14:59:18 +0100

    added sceleton for migration endpoint; had to open new api namespace in order to use 'old' auth method!

--- src/com/qdacity/Constants.java
+++ src/com/qdacity/Constants.java
@@ -2,6 +2,6 @@ package com.qdacity;
 
 public class Constants {
 	public static final String VERSION = "v10";
-	
+ 	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
 	public static final String WEB_CLIENT_ID = "385639345148-k8ph3ug699i8974d8f8vjcffd1tr9llg.apps.googleusercontent.com";
 }
---
+++ src/com/qdacity/endpoint/UserMigrationEndpoint.java
@@ -0,0 +1,113 @@
+package com.qdacity.endpoint;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+
+import javax.jdo.PersistenceManager;
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+
+import org.datanucleus.store.query.Query;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.response.UnauthorizedException;
+import com.qdacity.Cache;
+import com.qdacity.Constants;
+import com.qdacity.PMF;
+import com.qdacity.authentication.AuthenticatedUser;
+import com.qdacity.authentication.GoogleIdTokenValidator;
+import com.qdacity.user.User;
+import com.qdacity.user.UserLoginProviderInformation;
+import com.qdacity.user.UserType;
+
+/**
+ * Endpoints for migrating Users from older state to latest state.
+ */
+@Api(
+	name = "qdacityusermigration",
+	version = Constants.VERSION,
+	namespace = @ApiNamespace(
+		ownerDomain = "qdacity.com",
+		ownerName = "qdacity.com",
+		packagePath = "server.project")
+)
+public class UserMigrationEndpoint {
+
+	/**
+	 * Migrates a user from an old Google (Identity / Appengine) account to a "normal" Google account.
+	 * @param oldUser
+	 * @param idToken
+	 * @throws UnauthorizedException 
+	 */
+	@ApiMethod(
+			name = "userMigration.migrateFromGoogleIdentityToCustomAuthentication",
+			scopes = { Constants.EMAIL_SCOPE },
+			clientIds = { Constants.WEB_CLIENT_ID, com.google.api.server.spi.Constant.API_EXPLORER_CLIENT_ID },
+			audiences = { Constants.WEB_CLIENT_ID })
+	public void migrateFromGoogleIdentityToCustomAuthentication(com.google.appengine.api.users.User oldUser, @Named("idToken") String idToken) throws UnauthorizedException {
+		
+		if(oldUser == null) {
+			throw new UnauthorizedException("The token for the current user could not be validated.");
+		}
+		java.util.logging.Logger.getLogger("logger").log(Level.INFO, "old user id " + oldUser.getUserId());
+		java.util.logging.Logger.getLogger("logger").log(Level.INFO, "old user email " + oldUser.getEmail());
+		java.util.logging.Logger.getLogger("logger").log(Level.INFO, "old user federate id " + oldUser.getFederatedIdentity());
+		AuthenticatedUser newUser = new GoogleIdTokenValidator().validate(idToken);
+		if(newUser == null) {
+			throw new UnauthorizedException("The token for the new user could not be validated.");
+		}
+		java.util.logging.Logger.getLogger("logger").log(Level.INFO, "new user id " + newUser.getId());
+	}
+
+	
+	@ApiMethod(
+			name = "insertOldUserForTesting",
+			scopes = { Constants.EMAIL_SCOPE },
+			clientIds = { Constants.WEB_CLIENT_ID, com.google.api.server.spi.Constant.API_EXPLORER_CLIENT_ID },
+			audiences = { Constants.WEB_CLIENT_ID })
+	public User insertUser(User user, com.google.appengine.api.users.User loggedInUser) throws UnauthorizedException {
+		
+		if(loggedInUser == null) {
+			throw new UnauthorizedException("The User could not be authenticated");
+		}
+		
+		user.setId(loggedInUser.getUserId());
+		user.setProjects(new ArrayList<Long>());
+		user.setCourses(new ArrayList<Long>());
+		user.setType(UserType.USER);
+		user.setLastLogin(new Date());
+		PersistenceManager mgr = getPersistenceManager();
+		try {
+			if (user.getId() != null && containsUser(user)) {
+				throw new EntityExistsException("Object already exists");
+			}
+			mgr.makePersistent(user);
+		} finally {
+			mgr.close();
+		}
+		return user;
+	}
+	private boolean containsUser(User user) {
+		PersistenceManager mgr = getPersistenceManager();
+		boolean contains = true;
+		try {
+			mgr.getObjectById(User.class, user.getId());
+		} catch (javax.jdo.JDOObjectNotFoundException ex) {
+			contains = false;
+		} finally {
+			mgr.close();
+		}
+		return contains;
+	}
+
+
+	private static PersistenceManager getPersistenceManager() {
+		return PMF.get().getPersistenceManager();
+	}
+}
