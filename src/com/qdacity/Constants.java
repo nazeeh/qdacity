edab96384c02b15726db83c18aa8513f62b9a762
Robin Kreuzer <Robin.Kreuzer@fau.de>
2017-12-12 15:00:32 +0100

    fixed Constants.java for own env

--- src/com/qdacity/Constants.java
+++ src/com/qdacity/Constants.java
@@ -3,5 +3,5 @@ package com.qdacity;
 public class Constants {
 	public static final String WEB_CLIENT_ID = "227642428686-mlk1of1dvk369tajcm8hu7kstjtmfv2a.apps.googleusercontent.com";
  	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
-	public static final String VERSION = "v10";
+	public static final String VERSION = "v9";
 }
