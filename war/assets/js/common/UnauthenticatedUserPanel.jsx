6bb0739764640550607f5d32aecb7eeadf910673
Georg <georg.schwarz@fau.de>
2018-01-02 11:34:31 +0100

    added loading circle with default timeout 2s for UnauthenticatedUserPanel

--- war/assets/js/common/UnauthenticatedUserPanel.jsx
+++ war/assets/js/common/UnauthenticatedUserPanel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import SigninWithGoogleBtn from '../pages/index/SigninWithGoogleBtn.jsx';
 
@@ -20,18 +20,57 @@ const StyledPanel = styled.div `
 	}
     `;
 
+
+const loadingAnimation = keyframes `
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+`;
+const StyledLoader = styled.div `
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 250px;
+    margin-bottom:20px;
+    
+    width: 150px;
+    height: 150px;
+
+    border: 20px solid #f2f2f2;
+    border-radius: 50%;
+    border-top: 20px solid #428bca;
+
+    animation: ${loadingAnimation} 2s linear infinite;
+`;
+
 export default class UnauthenticatedUserPanel extends React.Component {
 	constructor(props) {
-		super(props);
+        super(props);
+        
+        this.state = {
+            loading: true
+        }
+        const timeout = props.timeout || 2000;
+
+        const _this = this;
+        setTimeout(function() {
+            _this.state.loading = false
+            _this.setState(_this.state);
+        }, timeout)
     }
     
 	render() {
-		return (
-           <StyledPanel>
-                <h4>You are currently not logged in!</h4>
-                <p>Please sign-in or register to access this page!</p>
-                <p>Click <a href="/">here</a> to get to the Home.</p>
-            </StyledPanel>
-        );
+        if(this.state.loading) {
+            return (
+                <StyledLoader/>
+            );
+        }
+        else {
+            return (
+                <StyledPanel>
+                        <h4>You are currently not logged in!</h4>
+                        <p>Please sign-in or register to access this page!</p>
+                        <p>Click <a href="/">here</a> to get to the Home.</p>
+                    </StyledPanel>
+                );
+            }
 	}
 }
