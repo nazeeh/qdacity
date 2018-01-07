99b32b51d0ae65b7333755cd4ccd1bbea3e0747a
Felix Loos <loos.felix93@gmail.com>
2018-01-07 02:33:29 +0100

    Implemented new List/Pagination/SearchBox for CourseList

--- war/assets/js/pages/personal-dashboard/CourseList.jsx
+++ war/assets/js/pages/personal-dashboard/CourseList.jsx
@@ -12,55 +12,33 @@ import CustomForm from '../../common/modals/CustomForm';
 import Confirm from '../../common/modals/Confirm';
 
 import {
-	StyledBoxList,
-	StyledPagination,
-	StyledPaginationItem,
+	ItemList,
+	ListMenu,
 	StyledListItemBtn,
 	StyledListItemPrimary,
 	StyledListItemDefault,
-} from '../../common/styles/List';
+} from '../../common/styles/ItemList.jsx';
 
-import StyledSearchField from '../../common/styles/SearchField.jsx';
 import {
 	BtnDefault
 } from '../../common/styles/Btn.jsx';
 
-const StyledNewPrjBtn = styled.div `
+const StyledNewCourseBtn = styled.div `
 	padding-left: 5px;
 `;
-
-const StyledProjectListMenu = styled.div `
-	display:flex;
-	flex-direction:row;
-	& > .searchfield{
-		height: inherit !important;
-		flex:1;
-	}
-`;
-
-
-const StyledProjectList = StyledBoxList.extend `
-	padding-top: 5px;
-`;
-
-
 
 export default class CourseList extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			// pagination
-			currentPage: 1,
-			itemsPerPage: 8,
-			search: '',
 			listStatus: []
 		};
 
+		this.itemList = null;
 
 		this.init();
 
-		this.paginationClick = this.paginationClick.bind(this);
-		this.updateSearch = this.updateSearch.bind(this);
+		this.renderCourse = this.renderCourse.bind(this);
 		this.showNewCourseModal = this.showNewCourseModal.bind(this);
 		this.createNewCourse = this.createNewCourse.bind(this);
 
@@ -183,14 +161,6 @@ export default class CourseList extends React.Component {
 		return courses;
 	}
 
-
-
-	paginationClick(event) {
-		this.setState({
-			currentPage: Number(event.target.id)
-		});
-	}
-
 	leaveCourse(e, course, index) {
 		var _this = this;
 		e.stopPropagation();
@@ -240,19 +210,6 @@ export default class CourseList extends React.Component {
 			this.props.history.push('/TermDashboard?termCourse=' + termCourseID);
 		}
 	}
-
-	updateSearch(e) {
-		this.setState({
-			search: e.target.value
-		});
-
-	}
-
-	isActivePage(page) {
-		return ((page == this.state.currentPage) ? 'active' : ' ');
-	}
-
-
 
 	showNewCourseModal() {
 		var _this = this;
@@ -315,109 +272,58 @@ export default class CourseList extends React.Component {
 		this.state.listStatus[this.state.listStatus.indexOf(courseIndex)].selectedTermCourseID = termCourse.id;
 	}
 
-	render() {
-		var _this = this;
-
-
-		//Render Components
-
-		//Render search and newPrjBtn
-
-		const projectListMenu = <StyledProjectListMenu>
-
-
-			<StyledSearchField className="searchfield" id="searchform">
-				<input
-					type="text"
-					placeholder="Search"
-					value={this.state.search}
-					onChange={this.updateSearch}
-				/>
-				<StyledNewPrjBtn id="newProject">
-					<BtnDefault
-						id="newPrjBtn"
-						href="#"
-						onClick={this.showNewCourseModal}
-
-					>
-					<i className="fa fa-plus fa-fw"></i>
-					New Course
-					</BtnDefault>
-				</StyledNewPrjBtn>
-
-			</StyledSearchField>
-
-		</StyledProjectListMenu>
-
-		//Rebder List Items
-		var filteredList = this.props.courses.filter(
-			(course) => {
-				return course.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
-			}
-		);
-		const lastItem = this.state.currentPage * this.state.itemsPerPage;
-		const firstItem = lastItem - this.state.itemsPerPage;
-		const itemsToDisplay = filteredList.slice(firstItem, lastItem);
-
-
-
-		const renderListItemContent = (course, index) => {
-
-			return ([
-				<span>{course.name}</span>,
-				<div>
-					<DropDownButton isListItemButton={true} items={course.terms} initText={this.defineInitText(course, index)}></DropDownButton>
-				<StyledListItemBtn onClick={(e) => this.deleteCourse(e, course, index)} className=" btn fa-lg" color={Theme.rubyRed} colorAccent={Theme.rubyRedAccent}>
-					<i className="fa fa-trash "></i>
-				</StyledListItemBtn>
-				<StyledListItemBtn onClick={(e) => this.configureCourse(e, course, index)} className=" btn fa-lg" color={Theme.darkGreen} colorAccent={Theme.darkGreenAccent}>
-					<i className="fa fa-cog "></i>
-				</StyledListItemBtn>
-				<StyledListItemBtn onClick={(e) => this.leaveCourse(e, course, index)} className=" btn fa-lg" color={Theme.rubyRed} colorAccent={Theme.rubyRedAccent}>
-					<i className="fa fa-sign-out"></i>
-				</StyledListItemBtn>
-
-			</div>
-			])
-		}
-		const renderListItems = itemsToDisplay.map((course, index) => {
-			return <StyledListItemPrimary key={course.id} onClick={() => this.courseClick(course, index)} clickable={true}>
-						{renderListItemContent(course, index)}
-					</StyledListItemPrimary>;
-
-		})
-
-		//Render Pagination
-		const pageNumbers = [];
-		for (let i = 1; i <= Math.ceil(this.props.courses.length / this.state.itemsPerPage); i++) {
-			pageNumbers.push(i);
-		}
-		const renderPagination = pageNumbers.map(pageNo => {
-			return (
-				<StyledPaginationItem
-	              key={pageNo}
-	              id={pageNo}
-	              onClick={this.paginationClick}
-	              className= {this.isActivePage(pageNo)}
-	            >
-	              {pageNo}
-			  </StyledPaginationItem>
-			);
-		});
-
-		return (
+	renderCourseContent(course, index) {
+		return ([
+			<span>{course.name}</span>,
 			<div>
+                <DropDownButton isListItemButton={true} items={course.terms} initText={this.defineInitText(course, index)}></DropDownButton>
+                <StyledListItemBtn onClick={(e) => this.deleteCourse(e, course, index)} className=" btn fa-lg" color={Theme.rubyRed} colorAccent={Theme.rubyRedAccent}>
+                    <i className="fa fa-trash "></i>
+                </StyledListItemBtn>
+                <StyledListItemBtn onClick={(e) => this.configureCourse(e, course, index)} className=" btn fa-lg" color={Theme.darkGreen} colorAccent={Theme.darkGreenAccent}>
+                    <i className="fa fa-cog "></i>
+                </StyledListItemBtn>
+                <StyledListItemBtn onClick={(e) => this.leaveCourse(e, course, index)} className=" btn fa-lg" color={Theme.rubyRed} colorAccent={Theme.rubyRedAccent}>
+                    <i className="fa fa-sign-out"></i>
+                </StyledListItemBtn>
+            </div>
+		]);
+	}
 
-				{projectListMenu}
-				<StyledProjectList className="">
-					{renderListItems}
-	            </StyledProjectList>
-	            <StyledPagination className="pagination">
-					{renderPagination}
-            	</StyledPagination>
-     		</div>
+	renderCourse(course, index) {
+		return (
+			<StyledListItemPrimary key={course.id} onClick={() => this.courseClick(course, index)} clickable={true}>
+                { this.renderCourseContent(course, index) }
+            </StyledListItemPrimary>
 		);
 	}
 
-
+	render() {
+		return (
+			<div>
+                <ListMenu>
+                    { this.itemList ? this.itemList.renderSearchBox() : '' }
+            
+                    <StyledNewCourseBtn id="newProject">
+                        <BtnDefault
+                            id="newPrjBtn"
+                            href="#"
+                            onClick={this.showNewCourseModal}>
+                            <i className="fa fa-plus fa-fw"></i>
+                            New Course
+                        </BtnDefault>
+                    </StyledNewCourseBtn>
+                </ListMenu>
+                        
+				<ItemList 
+                    ref={(r) => {if (r) this.itemList = r}}
+                    hasSearch={true}
+                    hasPagination={true}
+                    doNotrenderSearch={true}
+                    itemsPerPage={8}
+                    items={this.props.courses} 
+                    renderItem={this.renderCourse} />	
+     		</div>
+		);
+	}
 }
