8c42ec985d413e97c849960b3e5c7991a86b56d2
Felix Loos <loos.felix93@gmail.com>
2018-01-07 14:30:25 +0100

    Implemented new List/Pagination/SearchBox for ValidationProjectList

--- war/assets/js/pages/project-dashboard/RevisionHistory/ValPrjList.jsx
+++ war/assets/js/pages/project-dashboard/RevisionHistory/ValPrjList.jsx
@@ -5,49 +5,28 @@ import Theme from '../../../common/styles/Theme.js';
 import ProjectEndpoint from '../../../common/endpoints/ProjectEndpoint';
 
 import {
-	StyledPagination,
-	StyledPaginationItem,
+	ItemList,
+	ListMenu,
 	StyledListItemBtn,
-	StyledBoxList,
-	StyledListItemDefault
-} from '../../../common/styles/List';
+	StyledListItemPrimary,
+	StyledListItemDefault,
+} from '../../../common/styles/ItemList.jsx';
 
-import StyledSearchField from '../../../common/styles/SearchField.jsx';
 import {
 	BtnDefault
 } from '../../../common/styles/Btn.jsx';
 
-
 export default class ValPrjList extends React.Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
-			validationProjects: this.props.validationProjects,
-			// pagination
-			currentPage: 1,
-			itemsPerPage: 8,
-			search: ''
+			validationProjects: this.props.validationProjects
 		};
 
-		this.paginationClick = this.paginationClick.bind(this);
-		this.updateSearch = this.updateSearch.bind(this);
-	}
+		this.itemList = null;
 
-	paginationClick(event) {
-		this.setState({
-			currentPage: Number(event.target.id)
-		});
-	}
-
-	updateSearch(e) {
-		this.setState({
-			search: e.target.value
-		});
-
-	}
-
-	isActivePage(page) {
-		return (page == this.state.currentPage);
+		this.renderValidationProject = this.renderValidationProject.bind(this);
 	}
 
 	deleteValPrj(e, valPrjId, index) {
@@ -73,8 +52,8 @@ export default class ValPrjList extends React.Component {
 	renderDeleteBtn(valPrj, index) {
 		if (this.props.isAdmin || this.props.isProjectOwner)
 			return <StyledListItemBtn onClick={(e) => this.deleteValPrj(e,valPrj.id, index)} className="btn fa-lg"  color={Theme.rubyRed} colorAccent={Theme.rubyRedAccent}>
-						<i className="fa fa-trash"></i>
-					</StyledListItemBtn>;
+                        <i className="fa fa-trash"></i>
+                    </StyledListItemBtn>;
 		else return '';
 	}
 
@@ -82,80 +61,35 @@ export default class ValPrjList extends React.Component {
 		if (this.props.isAdmin || this.props.isProjectOwner) this.props.history.push('/CodingEditor?project=' + valPrjId + '&type=VALIDATION');
 	}
 
-	render() {
-		var _this = this;
-
-		//Render Components
-
-		//Render search and newPrjBtn
-		const renderSearch = <div>
-			<StyledSearchField className="searchfield" id="searchform">
-				<input
-					type="text"
-					placeholder="Search"
-					value={this.state.search}
-					onChange={this.updateSearch}
-				/>
-				<BtnDefault type="button">
-					<i className="fa fa-search  fa-lg"></i>
-				</BtnDefault>
-			</StyledSearchField>
-
-		</div>
-
-		// Render list items
-		var filteredList = this.state.validationProjects.filter(
-			(project) => {
-				return project.creatorName.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
-			}
-		);
-
-		const lastItem = this.state.currentPage * this.state.itemsPerPage;
-		const firstItem = lastItem - this.state.itemsPerPage;
-		const itemsToDisplay = filteredList.slice(firstItem, lastItem);
-
-		function prjClick(prj) {
-			console.log('Link');
-		}
-
-		const renderListItems = itemsToDisplay.map((valPrj, index) => {
-
-			return <StyledListItemDefault key={valPrj.id} onClick={() => this.valPrjLink(valPrj.id)} clickable={true}>
-					<span> {valPrj.creatorName} </span>
-					{this.renderDeleteBtn(valPrj, index)}
-				</StyledListItemDefault>;
-		})
-
-		//Render Pagination
-		const pageNumbers = [];
-		for (let i = 1; i <= Math.ceil(this.state.validationProjects.length / this.state.itemsPerPage); i++) {
-			pageNumbers.push(i);
-		}
-		const renderPagination = pageNumbers.map(pageNo => {
-			return (
-				<StyledPaginationItem
-	              key={pageNo}
-	              id={pageNo}
-	              onClick={this.paginationClick}
-	              active= {this.isActivePage(pageNo)}
-	            >
-	              {pageNo}
-			  </StyledPaginationItem>
-			);
-		});
-
+	renderValidationProject(valPrj, index) {
 		return (
-			<div>
-				{renderSearch}
-				<StyledBoxList>
-					{renderListItems}
-	            </StyledBoxList>
-	            <StyledPagination>
-					{renderPagination}
-            	</StyledPagination>
-     		</div>
+			<StyledListItemDefault key={valPrj.id} onClick={() => this.valPrjLink(valPrj.id)} clickable={true}>
+                <span> {valPrj.creatorName} </span>
+                {this.renderDeleteBtn(valPrj, index)}
+            </StyledListItemDefault>
 		);
 	}
 
-
+	render() {
+		return (
+			<div>
+                <ListMenu>
+                    { this.itemList ? this.itemList.renderSearchBox() : '' }
+            
+                    <BtnDefault type="button">
+                        <i className="fa fa-search  fa-lg"></i>
+                    </BtnDefault>
+                </ListMenu>
+                        
+                <ItemList 
+                    ref={(r) => {if (r) this.itemList = r}}
+                    hasSearch={true}
+                    hasPagination={true}
+                    doNotrenderSearch={true}
+                    itemsPerPage={8}
+                    items={this.state.validationProjects} 
+                    renderItem={this.renderValidationProject} />
+            </div>
+		);
+	}
 }
