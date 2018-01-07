453d7e60fbc132d386b8b53f5097dcafa38659e3
Felix Loos <loos.felix93@gmail.com>
2018-01-07 17:15:01 +0100

    Implemented new List/Pagination/SearchBox for TermCourseDashboard ExerciseList

--- war/assets/js/pages/termCourse-dashboard/Exercises/ExerciseList.jsx
+++ war/assets/js/pages/termCourse-dashboard/Exercises/ExerciseList.jsx
@@ -4,26 +4,23 @@ import CourseEndpoint from '../../../common/endpoints/CourseEndpoint';
 import ExerciseEndpoint from '../../../common/endpoints/ExerciseEndpoint';
 
 import {
-	StyledBoxList,
-	StyledPagination,
-	StyledPaginationItem,
+	ItemList,
+	ListMenu,
 	StyledListItemBtn,
-	StyledListItemDefault
-} from '../../../common/styles/List';
+	StyledListItemPrimary,
+	StyledListItemDefault,
+} from '../../../common/styles/ItemList.jsx';
 
 export default class ExerciseList extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			exercises: [],
-			// pagination
-			currentPage: 1,
-			itemsPerPage: 5
+			exercises: []
 		};
 
 		this.init();
 
-		this.paginationClick = this.paginationClick.bind(this);
+		this.renderExercise = this.renderExercise.bind(this);
 	}
 
 	init() {
@@ -40,75 +37,23 @@ export default class ExerciseList extends React.Component {
 		});
 	}
 
-
-
-
-	paginationClick(event) {
-		this.setState({
-			currentPage: Number(event.target.id)
-		});
-	}
-
-
-	isActivePage(page) {
-		return (page == this.state.currentPage);
-	}
-
-	renderPaginationIfNeccessary() {
-		if (this.state.exercises.length <= this.state.itemsPerPage) {
-			return '';
-		} else {
-			//Render Pagination
-			const pageNumbers = [];
-			for (let i = 1; i <= Math.ceil(this.state.exercises.length / this.state.itemsPerPage); i++) {
-				pageNumbers.push(i);
-			}
-			const renderPaginationItems = pageNumbers.map(pageNo => {
-				return (
-					<StyledPaginationItem
-		              key={pageNo}
-		              id={pageNo}
-		              onClick={this.paginationClick}
-		              active= {this.isActivePage(pageNo)}
-		            >
-		              {pageNo}
-				  </StyledPaginationItem>
-				);
-			});
-			return <StyledPagination key={"pagination"}>
-					{renderPaginationItems}
-            	</StyledPagination>
-		}
-
-	}
-
-	render() {
-		var _this = this;
-
-		//Render Components
-		const lastItem = this.state.currentPage * this.state.itemsPerPage;
-		const firstItem = lastItem - this.state.itemsPerPage;
-		const itemsToDisplay = this.state.exercises.slice(firstItem, lastItem);
-
-		function prjClick(prj) {}
-
-		const renderListItems = itemsToDisplay.map((exercise, index) => {
-			return <StyledListItemDefault key={index} className="clickable">
-					<span > {exercise.name} </span>
-				</StyledListItemDefault>;
-		})
-
-
-
+	renderExercise(exercise, index) {
 		return (
-			<div>
-				<StyledBoxList key={"itemList"}>
-					{renderListItems}
-	            </StyledBoxList>
-				{this.renderPaginationIfNeccessary()}
-     		</div>
+			<StyledListItemDefault key={index} className="clickable">
+                <span > {exercise.name} </span>
+            </StyledListItemDefault>
 		);
 	}
 
+	render() {
+		return (
+			<ItemList 
+                key={"itemlist"}
+                hasPagination={true}
+                itemsPerPage={8}
+                items={this.state.exercises} 
+                renderItem={this.renderExercise} />
+		);
+	}
 
 }
