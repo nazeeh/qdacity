ce8a601e542761c8fbe4dd017a06702e80984b16
Felix Loos <loos.felix93@gmail.com>
2018-01-07 23:32:54 +0100

    Replaced the SearchBox in ProjectSearch (Coding-Editor)

--- war/assets/js/pages/coding-editor/ProjectPanel/ProjectSearch.jsx
+++ war/assets/js/pages/coding-editor/ProjectPanel/ProjectSearch.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-import StyledSearchField from '../../../common/styles/SearchField.jsx';
+import {
+	SearchBox,
+	StyledSearchFieldContainer
+} from '../../../common/styles/SearchBox.jsx';
+
 import {
 	BtnDefault
 } from '../../../common/styles/Btn.jsx';
@@ -11,9 +15,10 @@ export default class ProjectSearch extends React.Component {
 		this.state = {
 			search: "",
 		};
-		this.input = {};
+		this.searchBox = {};
 		this.updateSearch = this.updateSearch.bind(this);
 		this.searchProject = this.searchProject.bind(this);
+		this.onKeyPress = this.onKeyPress.bind(this);
 	}
 
 	updateSearch(e) {
@@ -66,24 +71,27 @@ export default class ProjectSearch extends React.Component {
 	}
 
 	componentDidMount() {
-		this.input.focus();
+		this.searchBox.focus();
+	}
+
+	onKeyPress(e) {
+		if (e.key === 'Enter') {
+			this.searchProject();
+		}
 	}
 
 	render() {
 		return (
-			<StyledSearchField className="searchfield" id="searchform">
-				<input
-					ref={(c) => this.input = c}
-					type="text"
-					placeholder="Search for anything"
-					value={this.state.search}
-					onChange={this.updateSearch}
-					onKeyPress={(e) => { if (e.key === 'Enter') this.searchProject();}}
-				/>
+			<StyledSearchFieldContainer className="searchfield" id="searchform">
+		        <SearchBox 
+                    ref={(c) => this.searchBox = c} 
+                    placeholder="Search for anything"
+                    onSearch={this.updateSearch} 
+                    onKeyPress={this.onKeyPress} />  
 				<BtnDefault type="button" onClick={() => this.searchProject()}>
 					<i className="fa fa-search  fa-lg"></i>
 				</BtnDefault>
-			</StyledSearchField>
+			</StyledSearchFieldContainer>
 		);
 	}
 }
