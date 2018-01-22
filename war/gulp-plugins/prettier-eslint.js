27cf5fe68515584130ed55ba4f21d14b17750294
Andreas Kaufmann <andreas.kaufmann@fau.de>
2018-01-22 15:22:16 +0100

    Created our own gulp plugin for eslint-prettier The existing one had some pretty old dependencies, and some of the newer ESLint config did not work The two dependencies we actually need are now in our own package.json
    Removed line-break-style (for now) because it was previously not enforced, but now would be.

---
+++ war/gulp-plugins/prettier-eslint.js
@@ -0,0 +1,42 @@
+var through = require('through2');
+var prettierEslint = require('prettier-eslint');
+
+const options = {
+	eslintConfig: {
+		parserOptions: {
+			ecmaVersion: 7
+		},
+		rules: {
+			semi: ['error', 'never']
+		}
+	},
+	prettierOptions: {
+		bracketSpacing: true
+	}
+};
+
+module.exports = function() {
+	return through.obj(format);
+
+	function format(file, encoding, callback) {
+		if (file.isNull()) {
+			return callback(null, file);
+		}
+
+		if (file.isStream()) {
+			return callback(
+				new utils.PluginError('prettier-eslint', 'doesn\'t support Streams')
+			);
+		}
+
+		const sourceCode = file.contents.toString();
+		const formatted = prettierEslint({
+			filePath: file.path,
+			text: sourceCode
+		});
+
+		file.contents = new Buffer(formatted, encoding);
+
+		return callback(null, file);
+	}
+};
