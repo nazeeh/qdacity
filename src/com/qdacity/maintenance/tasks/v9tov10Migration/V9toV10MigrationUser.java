9b0600c071f1631f998f82a546bf28645b8289ef
Andreas Kaufmann <andreas.kaufmann@fau.de>
2018-01-30 19:07:44 +0100

    Added deferred task for migrating to v10 Added endpoint migration.migrateV9toV10 to trigger the migration which adds loginProviderInformations attribute with a null value to user entities if it doesn't already exist

---
+++ src/com/qdacity/maintenance/tasks/v9tov10Migration/V9toV10MigrationUser.java
@@ -0,0 +1,42 @@
+package com.qdacity.maintenance.tasks.v9tov10Migration;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.taskqueue.DeferredTask;
+import com.qdacity.project.metrics.EvaluationMethod;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class V9toV10MigrationUser implements DeferredTask {
+
+	@Override
+	public void run() {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Iterable<Entity> reports = getAll("User", datastore);
+		for (Entity user : reports) {
+			try {
+				// If the user does not already have the new attribute we need to add it
+				if (!user.hasProperty("loginProviderInformations")){
+					//set new attribut
+					user.setProperty("loginProviderInformations", null);
+					//Persist changes
+					datastore.put(user);
+				}
+
+			} catch (Exception e) {
+				//catch Exception, log it, then continue
+				Logger.getLogger("logger").log(Level.SEVERE, e.getMessage());
+			}
+		}
+	}
+
+	private static Iterable<Entity> getAll(String entityName, DatastoreService datastore) {
+		com.google.appengine.api.datastore.Query q = new com.google.appengine.api.datastore.Query(entityName);
+		PreparedQuery pq = datastore.prepare(q);
+
+		return pq.asIterable();
+	}
+
+}
