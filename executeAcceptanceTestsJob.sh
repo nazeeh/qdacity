accabc522f7fdc99c5e63d65b9f138fa6b056df7
Felix Loos <loos.felix93@gmail.com>
2018-02-02 16:11:19 +0100

    Added a sh file which is executed by the acceptance test ci job. The sh script starts the docker container. The container name contains the CI_PROJECT_ID now

---
+++ executeAcceptanceTestsJob.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "ALL CONTAINERS"
+docker container ls -a
+echo "RUNNING CONTAINERS"
+docker container ls
+echo "AVAILABLE IMAGES"
+docker image ls
+    
+echo "CLEAR EVERYTHING" 
+docker container stop $(docker container ls -a -q) || true
+docker container rm $(docker container ls -a -q) || true
+docker image rm $(docker image ls -a -q) || true
+
+echo "ALL CONTAINERS"
+docker container ls -a
+echo "RUNNING CONTAINERS"
+docker container ls
+echo "AVAILABLE IMAGES"
+docker image ls
+    
+echo "START BUILD"
+
+IMAGE_NAME="qdatest"+$CI_PROJECT_ID
+
+docker build -t $IMAGE_NAME .
+docker run -t -v /dev/shm:/dev/shm -v ~/.m2:/root/.m2 $IMAGE_NAME
