a710cc2e42c808d1b31595578cf3e4e40d9dee12
Felix Loos <loos.felix93@gmail.com>
2018-02-11 19:49:51 +0100

    Added some simple tests

---
+++ war/tests/acceptance-tests/SimpleTest.js
@@ -0,0 +1,34 @@
+var webdriver = require('selenium-webdriver'),
+	By = webdriver.By,
+	until = webdriver.until;
+var chrome = require("selenium-webdriver/chrome");
+
+describe('Simple test', function() {
+
+	var defaultTimeout = 40000;
+	
+	var driver = null;
+	
+    beforeEach((done) => {
+    	const options = new chrome.Options();
+    	
+        this.driver = new webdriver.Builder()
+	        .forBrowser('chrome')
+	        .withCapabilities(options.toCapabilities())
+	        .build();
+
+        this.driver.get('http://localhost:8888/').then(done);
+    }, defaultTimeout);
+
+    afterEach((done) => {
+        this.driver.quit().then(done);   
+    }, defaultTimeout);
+
+    it('The system is working', (done) => {
+		// Find login button
+		this.driver.wait(until.elementLocated(By.xpath("//button//i[contains(@class,'fa-google')]"))).then(() =>  {
+			expect(1).toBe(1);
+			done();
+		});
+    }, defaultTimeout);
+});
