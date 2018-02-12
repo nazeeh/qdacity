4f63a850f6c5db661942cd8ce2cc1692056cc514
Felix Loos <loos.felix93@gmail.com>
2018-02-12 02:17:12 +0100

    Fixed gitlab ci job

--- executeAcceptanceTestsJob.sh
+++ executeAcceptanceTestsJob.sh
@@ -1,56 +1,20 @@
 #!/bin/bash
 
-
-echo "======================================================================="
-echo "ALL CONTAINERS"
+echo "Existing container"
 docker container ls -a
 
-echo "============="
-echo "RUNNING CONTAINERS"
-docker container ls
-echo "============="
-echo "AVAILABLE IMAGES"
+echo "Existing images"
 docker image ls
 
-echo "======================================================================="
-    
-echo "CLEAR EVERYTHING" 
-#docker container stop $(docker container ls -a -q) || true
-#docker container rm $(docker container ls -a -q) || true
-#docker image rm $(docker image ls -a -q) || true
-
-
-# Remove dangling images
+echo "Clear dangling images" 
 docker rmi $(docker images -qa -f "dangling=true")
 
 
-
-   
-echo "START BUILD"
-
-IMAGE_NAME_BASE="qdacity-tests-base"
+# Build docker image
 IMAGE_NAME_TESTS="qdacity-tests-${CI_PROJECT_ID}"
-
-# Build base
-docker build -f ./docker/acceptance-tests/Dockerfile.base -t $IMAGE_NAME_BASE .
 
 # Build test image
 docker build -f ./docker/acceptance-tests/Dockerfile.tests -t $IMAGE_NAME_TESTS .
 
 # Run test image
 docker run --rm -v /dev/shm:/dev/shm $IMAGE_NAME_TESTS
-
-
-
-
-echo "======================================================================="
-echo "ALL CONTAINERS"
-docker container ls -a
-echo "============="
-echo "RUNNING CONTAINERS"
-docker container ls
-echo "============="
-echo "AVAILABLE IMAGES"
-docker image ls
-
-echo "======================================================================="
