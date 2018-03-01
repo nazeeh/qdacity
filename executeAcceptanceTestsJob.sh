2c9c4e166146f9d480c634363b535cab59be465b
Andreas Kaufmann <andreas.kaufmann@fau.de>
2018-03-01 14:03:59 +0100

    removing existing test image before re-creating it

--- executeAcceptanceTestsJob.sh
+++ executeAcceptanceTestsJob.sh
@@ -19,6 +19,9 @@ IMAGE_NAME_TESTS="qdacity-tests-${CI_PROJECT_ID}"
 # Build the base image if it does not exist
 [ ! -z $(docker images -q $BASE_IMAGE_TAG) ] || docker build -f ./docker/acceptance-tests/Dockerfile.base -t qdacity-tests-base .
 
+# Remove existing image
+docker rmi $IMAGE_NAME_TESTS
+
 # Build the test image
 docker build -f ./docker/acceptance-tests/Dockerfile.tests -t $IMAGE_NAME_TESTS .
 
