8609b290512fc792a9d6f56d1a72d977b538813e
Felix Loos <loos.felix93@gmail.com>
2018-03-12 19:03:11 +0100

    Removed unnecessary code and added --quiet flag

--- docker/acceptance-tests/entrypoint.sh
+++ docker/acceptance-tests/entrypoint.sh
@@ -10,10 +10,7 @@ sleep 5
 # Start the dev-server
 echo "Start the devserver"
 
-ls /usr/local/gcloud
-ls /usr/local/gcloud/google-cloud-sdk
-ls /usr/local/gcloud/google-cloud-sdk/bin
-sudo /usr/local/gcloud/google-cloud-sdk/bin/java_dev_appserver.sh --port=8888 ./target/qdacity-war/ &
+sudo /usr/local/gcloud/google-cloud-sdk/bin/java_dev_appserver.sh --quiet --port=8888 ./target/qdacity-war/ &
 
 # Run a script which starts the acceptance-tests as soon as the specified port is open (=> the dev-server is running)
 ./docker/acceptance-tests/startTests.sh
