6bad117d6c9e3498912a408d6add3ec9c6b2ca5a
Felix Loos <loos.felix93@gmail.com>
2018-03-12 13:45:34 +0100

    Fixed spelling error

--- docker/acceptance-tests/entrypoint.sh
+++ docker/acceptance-tests/entrypoint.sh
@@ -10,10 +10,10 @@ sleep 5
 # Start the dev-server
 echo "Start the devserver"
 
-ls /opt/google-clod-sdk
-ls /opt/google-clod-sdk/google-clod-sdk
-ls /opt/google-clod-sdk/google-clod-sdk/bin
-/opt/google-clod-sdk/google-clod-sdk/bin/java_dev_appserver.sh --port=8888 ./target/qdacity-war/ &
+ls /opt/google-cloud-sdk
+ls /opt/google-cloud-sdk/google-cloud-sdk
+ls /opt/google-cloud-sdk/google-cloud-sdk/bin
+/opt/google-cloud-sdk/google-cloud-sdk/bin/java_dev_appserver.sh --port=8888 ./target/qdacity-war/ &
 
 # Run a script which starts the acceptance-tests as soon as the specified port is open (=> the dev-server is running)
 ./docker/acceptance-tests/startTests.sh
