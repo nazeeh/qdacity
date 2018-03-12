5750427da1ac9cd6adbb2e7b446b07968d0abbcb
Felix Loos <loos.felix93@gmail.com>
2018-03-12 13:15:28 +0100

    Use google cloud sdk instead of appengine java sdk

--- docker/acceptance-tests/entrypoint.sh
+++ docker/acceptance-tests/entrypoint.sh
@@ -10,8 +10,7 @@ sleep 5
 # Start the dev-server
 echo "Start the devserver"
 
-/opt/appengine-java-sdk/appengine-java-sdk-1.9.62/bin/dev_appserver.sh --port=8888 ./target/qdacity-war/ &
-
+/opt/google-clod-sdk/google-clod-sdk/bin/java_dev_appserver.sh --port=8888 ./target/qdacity-war/ &
 
 # Run a script which starts the acceptance-tests as soon as the specified port is open (=> the dev-server is running)
 ./docker/acceptance-tests/startTests.sh
