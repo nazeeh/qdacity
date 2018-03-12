696d99bc4af9f80893656634c28fb7a1e5c65f69
Felix Loos <loos.felix93@gmail.com>
2018-03-12 13:35:30 +0100

    Added debugging

--- docker/acceptance-tests/entrypoint.sh
+++ docker/acceptance-tests/entrypoint.sh
@@ -10,6 +10,9 @@ sleep 5
 # Start the dev-server
 echo "Start the devserver"
 
+ls /opt/google-clod-sdk
+ls /opt/google-clod-sdk/google-clod-sdk
+ls /opt/google-clod-sdk/google-clod-sdk/bin
 /opt/google-clod-sdk/google-clod-sdk/bin/java_dev_appserver.sh --port=8888 ./target/qdacity-war/ &
 
 # Run a script which starts the acceptance-tests as soon as the specified port is open (=> the dev-server is running)
