6f9e1f03547a9b9184e0c116eb9c706d10c5031e
Felix Loos <loos.felix93@gmail.com>
2018-03-12 14:56:58 +0100

    Removed ENV gcloud

--- docker/acceptance-tests/entrypoint.sh
+++ docker/acceptance-tests/entrypoint.sh
@@ -10,6 +10,9 @@ sleep 5
 # Start the dev-server
 echo "Start the devserver"
 
+ls /usr/local/gcloud
+ls /usr/local/gcloud/google-cloud-sdk
+ls /usr/local/gcloud/google-cloud-sdk/bin
 sudo /usr/local/gcloud/google-cloud-sdk/bin/java_dev_appserver.sh --port=8888 ./target/qdacity-war/ &
 
 # Run a script which starts the acceptance-tests as soon as the specified port is open (=> the dev-server is running)
