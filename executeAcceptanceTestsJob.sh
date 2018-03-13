e5f571149db58d0133ddcd9c7d0cca279d529b0d
Felix Loos <loos.felix93@gmail.com>
2018-03-13 03:32:20 +0100

    Fixed variable name consistency

--- executeAcceptanceTestsJob.sh
+++ executeAcceptanceTestsJob.sh
@@ -24,15 +24,15 @@ docker rmi $(docker images -qa -f "dangling=true")
 
 
 BASE_IMAGE_TAG="qdacity-tests-base:latest"
-IMAGE_NAME_TESTS="qdacity-tests-${CI_PROJECT_ID}"
+TEST_IMAGE_TAG="qdacity-tests-${CI_PROJECT_ID}"
 
 # Build the base image if it does not exist
 [ ! -z $(docker images -q $BASE_IMAGE_TAG) ] || docker build -f ./docker/acceptance-tests/Dockerfile.base -t qdacity-tests-base .
 
 echo "Building the image" 
 # Build the test image
-docker build --no-cache -f ./docker/acceptance-tests/Dockerfile.tests -t $IMAGE_NAME_TESTS .
+docker build --no-cache -f ./docker/acceptance-tests/Dockerfile.tests -t $TEST_IMAGE_TAG .
  
 echo "Running the image" 
 # Run docker image
-docker run --rm -v /dev/shm:/dev/shm $IMAGE_NAME_TESTS
+docker run --rm -v /dev/shm:/dev/shm $TEST_IMAGE_TAG
