a811c34c021a3cde74c56d36240acdb2e7b9adfc
Andreas Kaufmann <andreas.kaufmann@fau.de>
2018-03-13 13:54:30 +0100

    Added Logger module, which writes to console and Stackdriver Logging

--- realtime-service/src/syncService/index.js
+++ realtime-service/src/syncService/index.js
@@ -1,10 +1,10 @@
 const Socket = require('./Socket.js');
+const Logger = require('../utils/Logger.js');
 
 // Listen for new socket connections and hand off to new Socket objects
 module.exports = (io, redis) => {
   io.on('connection', socket => {
-    console.info(`${socket.id} connected`);
-
+	Logger.info(`${socket.id} connected`);
     new Socket(io, socket, redis);
   });
 };
