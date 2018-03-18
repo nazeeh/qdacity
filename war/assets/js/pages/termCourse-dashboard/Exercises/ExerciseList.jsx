de8297c723091612a83ad13fd606558b9023579a
nazeeh <nazeehammari@Nazeehs-MBP.home>
2018-03-18 20:58:50 +0100

    Added a method to convert Java date to JavaScript date

--- war/assets/js/pages/termCourse-dashboard/Exercises/ExerciseList.jsx
+++ war/assets/js/pages/termCourse-dashboard/Exercises/ExerciseList.jsx
@@ -45,6 +45,7 @@ export default class ExerciseList extends React.Component {
 	}
 
 	renderExercise(exercise, index) {
+		console.log(this.deadlinePassed(exercise));
 		return (
 			<StyledListItemDefault key={index} className="clickable">
 				<span> {exercise.name} </span>
@@ -82,6 +83,48 @@ export default class ExerciseList extends React.Component {
 				);
 			}
 		});
+	}
+
+	javaToJSDate(javaDateString) {
+		var year;
+		var month;
+		var day;
+		var hour = 0;
+		var minute = 0;
+		var second = 0;
+		var millisecond = 0;
+		var JSExerciseDeadline = new Date();
+		if (typeof javaDateString != 'undefined') {
+			year = javaDateString.substr(0, javaDateString.indexOf("-"));
+			javaDateString = javaDateString.substr(javaDateString.indexOf("-")+1);
+			month = javaDateString.substr(0, javaDateString.indexOf("-"));
+			javaDateString = javaDateString.substr(javaDateString.indexOf("-")+1);
+			day = javaDateString.substr(0, javaDateString.indexOf("T"));
+			javaDateString = javaDateString.substr(javaDateString.indexOf("T")+1);
+			JSExerciseDeadline.setFullYear(year);
+			JSExerciseDeadline.setMonth(month - 1);
+			JSExerciseDeadline.setDate(day);
+			JSExerciseDeadline.setHours(hour);
+			JSExerciseDeadline.setMinutes(minute);
+			JSExerciseDeadline.setSeconds(second);
+			JSExerciseDeadline.setMilliseconds(millisecond);
+		}
+		else {
+			//If the deadline of the exercise is undefined in the datastore, set it to one year later here
+			JSExerciseDeadline.setFullYear(JSExerciseDeadline.getFullYear()+1);
+		}
+		return (JSExerciseDeadline);
+	}
+	deadlinePassed(exercise) {
+		var JSExerciseDeadline = this.javaToJSDate(exercise.exerciseDeadline);
+		var now = new Date();
+		if (JSExerciseDeadline < now) {
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	render() {
