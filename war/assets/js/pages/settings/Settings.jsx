b59fc1751f8d69adc7957ba45e37769a75878e80
Georg Schwarz <georg.schwarz@fau.de>
2018-03-27 08:49:10 +0200

    introduced full height sidebar to Settings

--- war/assets/js/pages/settings/Settings.jsx
+++ war/assets/js/pages/settings/Settings.jsx
@@ -1,9 +1,35 @@
 //@ts-check
 import React, {Component} from 'react';
 import { Switch, Route } from 'react-router'
+import styled from 'styled-components';
 import IntlProvider from '../../common/Localization/LocalizationProvider';
 
 import LocalizationSettingsPage from './LocalizationSettings.jsx';
+
+
+const GridContainer = styled.div`
+	padding-top: 51px;
+	display: grid;
+	grid-template-columns: 200px auto;
+	grid-template-areas:
+        'sidebarNav settingsContent';
+    height: 100vh;
+`;
+
+const SidebarNav = styled.div`
+    grid-area: sidebarNav;
+    height: 100fr;
+    background-color: ${props => props.theme.defaultPaneBg};
+	border-right: 1px solid ${props => props.theme.borderDefault};
+    overflow-y: auto;
+`;
+
+
+const SettingsContent = styled.div`
+    grid-area: settingsContent;
+    overflow-y: auto;
+`;
+
 
 export default class SettingsPage extends Component {
 	constructor(props) {
@@ -13,19 +39,26 @@ export default class SettingsPage extends Component {
 
     render() {
         return (
-            <Switch>
-                <Route
-                    path="/Settings/Localization"
-                    render={props => (
-                        <LocalizationSettingsPage
-                            locale={this.props.locale}
-                            language={this.props.language}
-                            messages={this.props.messages}
-                            auth={this.props.auth} 
+            <GridContainer>
+                <SidebarNav>
+                    Hallo123
+                </SidebarNav>
+                <SettingsContent>
+                    <Switch>
+                        <Route
+                            path="/Settings/Localization"
+                            render={props => (
+                                <LocalizationSettingsPage
+                                    locale={this.props.locale}
+                                    language={this.props.language}
+                                    messages={this.props.messages}
+                                    auth={this.props.auth} 
+                                />
+                            )}
                         />
-                    )}
-                />
-            </Switch>
+                    </Switch>
+                </SettingsContent>
+            </GridContainer>
         );
     }
 }
