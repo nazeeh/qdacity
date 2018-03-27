dd13cc344d39a9c97e416c06063383507854db84
Georg Schwarz <georg.schwarz@fau.de>
2018-03-27 08:25:24 +0200

    shifted LocalizationSettingsPage to own route (by subroute)

--- war/assets/js/pages/settings/Settings.jsx
+++ war/assets/js/pages/settings/Settings.jsx
@@ -1,5 +1,6 @@
 //@ts-check
 import React, {Component} from 'react';
+import { Switch, Route } from 'react-router'
 import IntlProvider from '../../common/Localization/LocalizationProvider';
 
 import LocalizationSettingsPage from './LocalizationSettings.jsx';
@@ -11,10 +12,20 @@ export default class SettingsPage extends Component {
     
 
     render() {
-        return <LocalizationSettingsPage
-                    locale={this.props.locale}
-                    language={this.props.language}
-                    messages={this.props.messages}
-                    auth={this.props.auth} />
+        return (
+            <Switch>
+                <Route
+                    path="/Settings/Localization"
+                    render={props => (
+                        <LocalizationSettingsPage
+                            locale={this.props.locale}
+                            language={this.props.language}
+                            messages={this.props.messages}
+                            auth={this.props.auth} 
+                        />
+                    )}
+                />
+            </Switch>
+        );
     }
 }
