c3a1519bd9b276a6bb4172cdd8af3794b0ff10a7
Georg Schwarz <georg.schwarz@fau.de>
2018-03-28 18:41:03 +0200

    first unstyled implementation of listview with retrieved login data

--- war/assets/js/pages/settings/LoginDataSettings.jsx
+++ war/assets/js/pages/settings/LoginDataSettings.jsx
@@ -1,14 +1,86 @@
 //@ts-check
 import React, {Component} from 'react';
 
+import IntlProvider from '../../common/Localization/LocalizationProvider';
+import { FormattedMessage } from 'react-intl';
+
+import styled from 'styled-components';
+
+
+const StyledPanel = styled.div`
+	background-color: ${props => props.theme.defaultPaneBg};
+	border: 1px solid ${props => props.theme.borderDefault};
+	padding: 20px 50px 20px 50px;
+	margin: 20px;
+	text-align: center;
+`;
+
+const StyledAssociatedLoginList = styled.ul``;
+
 export default class LoginDataSettings extends Component {
 	constructor(props) {
-		super(props);
+        super(props);
+
+        this.state = {
+            associatedLogins: []
+        }
+        
+        this.updateAssociatedLoginList = this.updateAssociatedLoginList.bind(this);
+        this.props.auth.authentication.addAuthStateListener(this.updateAssociatedLoginList);
+
+        this.updateAssociatedLoginList();
+    }
+
+    updateAssociatedLoginList() {
+        const _this = this;
+
+        gapi.client.qdacity.auth.getAssociatedLogins().execute(function(resp) {
+            if (!resp.code) {
+                _this.setState({
+                    associatedLogins: resp.items
+                });
+            } else {
+                console.error('Could not fetch any associated logins.');
+                _this.setState({
+                    associatedLogins: []
+                });
+            }
+        });
     }
     
     render() {
+
+        const AssociatedLoginListItems = ({ associatedLoginList }) => {
+            console.log(associatedLoginList);
+            return !! associatedLoginList ? <div>
+                {associatedLoginList.map((associatedLogin, i) => {
+                    return (
+                        <AssociatedLoginListItem associatedLogin={associatedLogin}/>
+                    );
+                })}
+            </div> : null;
+        }
+
+        const AssociatedLoginListItem = ({ associatedLogin }) => {
+            console.log(associatedLogin);
+            return (
+                <li>{associatedLogin.provider + ' | ' + associatedLogin.externalEmail}</li>
+            );
+        }
+
         return (
-            <p>Login Data Settings </p>
+            <StyledPanel>
+                <h2>
+                    <FormattedMessage
+                        id="settings.logindata.heading"
+                        defaultMessage="Login Data Settings"
+                    />	
+                </h2>        
+
+                <StyledAssociatedLoginList>
+                    <AssociatedLoginListItems associatedLoginList={this.state.associatedLogins}/>
+                </StyledAssociatedLoginList>
+            </StyledPanel>
         );
     }
 }
