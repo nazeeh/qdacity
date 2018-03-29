1b49f5fc411b4163bf0c32badf6f349db02c80c2
Felix Loos <loos.felix93@gmail.com>
2018-03-29 17:49:32 +0200

    -- Restored local settings

--- src/com/qdacity/Constants.java
+++ src/com/qdacity/Constants.java
@@ -3,6 +3,6 @@ package com.qdacity;
 public class Constants {
 	public static final String WEB_CLIENT_ID = "62776117166-iuu5qlid44hnas1ivvurlr0cku1f0hnc.apps.googleusercontent.com";
   	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email"; 	
-	public static final String VERSION = "v1";
+	public static final String VERSION = "2";
 	public static final String BILLING_TABLE = "qdacity-app.qdacity_billing.gcp_billing_export_v1_005083_BC6F93_4D2352";
 }
