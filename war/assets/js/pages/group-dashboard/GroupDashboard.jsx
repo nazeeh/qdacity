6510a38c6cf5031d01002f03862acbf4dec2868a
Georg Schwarz <georg.schwarz@fau.de>
2018-03-31 13:54:56 +0200

    added heading to GroupDashboard

--- war/assets/js/pages/group-dashboard/GroupDashboard.jsx
+++ war/assets/js/pages/group-dashboard/GroupDashboard.jsx
@@ -1,9 +1,36 @@
 //@ts-check
 import React, {Component} from 'react';
 
+import { FormattedMessage } from 'react-intl';
+
+import styled from 'styled-components';
+
 import UserGroupEndpoint from '../../common/endpoints/UserGroupEndpoint.js';
 
 import UnauthenticatedUserPanel from '../../common/UnauthenticatedUserPanel.jsx';
+
+
+const StyledDashboard = styled.div`
+    margin-top: 35px;
+    
+    display: grid;
+    grid-gap: 20px;
+    grid-template-areas:
+        "header header header"
+        "projects courses users"
+`;
+
+const StyledPageHeader = styled.div`
+    grid-area: header;
+
+    padding-left: 20px;
+`;
+
+const StyledUserGroupName = styled.span`
+    margin-left: 5px;
+`;
+
+
 
 export default class GroupDashboard extends Component {
 	constructor(props) {
@@ -12,7 +39,9 @@ export default class GroupDashboard extends Component {
         const urlParams = URI(window.location.search).query(true);
 
         this.state = {
-            userGroup: null,
+            userGroup: {
+                name: ''
+            },
             isOwner: false,
             isParticipant: false
         }
@@ -39,7 +68,12 @@ export default class GroupDashboard extends Component {
         }
         
         return (
-            <p>GroupDashboard</p>
+            <StyledDashboard>
+                <StyledPageHeader className="page-header">
+                    <i className="fa fa-users" />
+                    <StyledUserGroupName>{this.state.userGroup.name}</StyledUserGroupName>
+                </StyledPageHeader>
+			</StyledDashboard>
         );
     }
 }
