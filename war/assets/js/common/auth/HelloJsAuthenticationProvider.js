2cb40f08513951a3f98508c02ed8dcb2bdc15503
Georg Schwarz <georg.schwarz@fau.de>
2018-04-01 18:41:58 +0200

    init facebook on hellojs

--- war/assets/js/common/auth/HelloJsAuthenticationProvider.js
+++ war/assets/js/common/auth/HelloJsAuthenticationProvider.js
@@ -7,11 +7,14 @@ import ImageUtil from './ImageUtil.js';
 
 const GOOGLE_CLIENT_ID = '$CLIENT_ID$';
 const TWITTER_CLIENT_ID = '$TWITTER_CLIENT_ID$';
+//const FACEBOOK_CLIENT_ID = '$FACEBOOK_CLIENT_ID$';
+const FACEBOOK_CLIENT_ID = '187499018555928';
     
     
 // hellojs for 'regular' sign-in
 hello.init({
 	google: GOOGLE_CLIENT_ID,
+	facebook: FACEBOOK_CLIENT_ID,
 	twitter: TWITTER_CLIENT_ID
 }, {
     redirect_uri: '$APP_PATH$'
