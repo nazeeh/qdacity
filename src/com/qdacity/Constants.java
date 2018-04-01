8c3ff40c5835ad50ecba241f4852ffd0cb3c80dc
Georg Schwarz <georg.schwarz@fau.de>
2018-04-01 11:17:39 +0200

    added empty TwitterTokenValidator with required constants

--- src/com/qdacity/Constants.java
+++ src/com/qdacity/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
 	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
 	public static final String VERSION = "v15";
 	public static final String BILLING_TABLE = "qdacity-app.qdacity_billing.gcp_billing_export_v1_005083_BC6F93_4D2352";
+
+	public static final String TWITTER_CONSUMER_KEY = "";
+	public static final String TWITTER_CONSUMER_SECRET = "";
 }
---
+++ src/com/qdacity/authentication/TwitterTokenValidator.java
@@ -0,0 +1,9 @@
+package com.qdacity.authentication;
+
+public class TwitterTokenValidator implements TokenValidator {
+
+    @Override
+    public AuthenticatedUser validate(String token) {
+        return null;
+    }
+}
