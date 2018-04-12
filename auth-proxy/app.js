f7b177c1c3a79248a2028c00774da2ad621a2141
Andreas Kaufmann <andreas.kaufmann@fau.de>
2018-04-12 14:14:33 +0200

    Added twitter credentials

--- auth-proxy/app.js
+++ auth-proxy/app.js
@@ -8,10 +8,10 @@ app.all('/oauthproxy', oauthshim);
 // Initiate the shim with Client ID's and secret, e.g.
 oauthshim.init([{
 	// id : secret
-	client_id: '12345',
-	client_secret: 'secret678910',
+	client_id: '8WjTuE2g5hc5FJA4nsY5ykXCa',
+	client_secret: 'XOgdbf7LN79OmIsIBAT7jnzqU4AfIRtaFIYEBfQCyfVopoxH0N',
 	// Define the grant_url where to exchange Authorisation codes for tokens
-	grant_url: 'https://linkedIn.com',
+	grant_url: 'https://api.twitter.com/oauth/access_token',
 	// Restrict the callback URL to a delimited list of callback paths
 	domain: 'appspot.com, qdacity.com/redirect'
 }
