fa1ed10f13f26aad7e0d20548cba222c178e7626
Andreas Kaufmann <andreas.kaufmann@fau.de>
2018-04-13 17:22:50 +0200

    Added timestamp output for qdacity load test

--- war/tests/acceptance-tests/00_LoadQDAcityTest.js
+++ war/tests/acceptance-tests/00_LoadQDAcityTest.js
@@ -17,18 +17,21 @@ describe(SPEC_NAME, function () {
     });
 	
     beforeEach((done) => {
+		console.log("Starting test at " + new Date() );
     	this.driver = Common.setupChromeDriver();
         this.driver.get('http://localhost:8888/').then(done);
     });
 
     afterEach((done) => {
-        this.driver.quit().then(done);   
+        this.driver.quit().then(done);
+		console.log("Ending test at " + new Date() );
     });
 
     it('The system is running', (done) => {
 		// Find login button
 		this.driver.wait(until.elementLocated(By.xpath("//button//i[contains(@class,'fa-sign-in')]"))).then(() =>  {
 			console.log('Found the login button. Test passed!');
+			console.log("Found button at " + new Date() );
 			done();
 		});
 	}, Common.getDefaultTimeout());
