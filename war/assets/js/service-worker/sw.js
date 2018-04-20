fa161b856d321375345f54268e1f1ee24a5b1f41
Sebastian Knauer <sebastian.knauer@fau.de>
2018-04-20 14:45:40 +0200

    add route function

--- war/assets/js/service-worker/sw.js
+++ war/assets/js/service-worker/sw.js
@@ -1,23 +1,24 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-import {get, set} from 'idb-keyval';
 
-// TODO replace v14 with version
-function fetchDiscoveryDoc() {
-	fetch('_ah/api/discovery/v1/apis/qdacity/v14/rest').then(function (response) {
-		if (response.ok) {
-			return response.json();
-		}
-	})
+let apiMethods = {};
+
+function discoverApi() {
+	return fetch('_ah/api/discovery/v1/apis/qdacity/$API_VERSION$/rest')
 		.then(function (response) {
-			parseDiscoveryDoc(response);
-		}).catch(function (error) {
-		console.log("error discovery ", error)
-
-	});
+			if (response.ok) {
+				return response.json();
+			}
+		})
+		.then(function (response) {
+			return _parseDiscoveryDoc(response);
+		})
+		.catch(function (error) {
+			console.log("error discovery ", error)
+		});
 }
 
-function parseDiscoveryDoc(discovery) {
+function _parseDiscoveryDoc(discovery) {
 	console.log(discovery);
 	const resources = discovery.resources;
 	for (let resource in resources) {
@@ -25,10 +26,11 @@ function parseDiscoveryDoc(discovery) {
 		for (let method in resource.methods) {
 			method = resource.methods[method];
 			if (method.httpMethod === "POST") {
-				set(method.id, (discovery.basePath + method.path).replace(/^\/+/g, ''));
+				apiMethods[method.id] = (discovery.basePath + method.path).replace(/^\/+/g, '');
 			}
 		}
 	}
+	registerPostRoutes();
 }
 
 function pathToRegex(path) {
@@ -39,7 +41,7 @@ function pathToRegex(path) {
 
 self.addEventListener('install', function (event) {
 	console.log('[ServiceWorker] installing.');
-	event.waitUntil(fetchDiscoveryDoc());
+	event.waitUntil(discoverApi());
 });
 
 workbox.routing.registerRoute(
@@ -70,15 +72,59 @@ const insertProjectHandler = ({url, event}) => {
 		.catch(function (error) {
 			console.warn('[ServiceWorker|Post] Error from fetch: ', error);
 		});
-	//return new Response(`Custom handler response.`);
+	const testProject = {
+		codesystemID: "1234",
+		description: "offline created",
+		id: "12345678",
+		maxCodingID: "0",
+		name: "My first offline projekt",
+		revision: 0,
+		type: "PROJECT"
+	};
+	return new Response(JSON.stringify(testProject));
+};
+
+const insertProjectHandler = ({url, event}) => {
+	console.log('[Workbox] insert Porject handler called');
+	return fetch(event.request)
+		.then(function (response) {
+			if (!response) {
+				console.log(
+					'[ServiceWorker|POST] No response from fetch ',
+					event.request.url
+				);
+				return response;
+			}
+			console.log(
+				'[ServiceWorker|POST] Good Response from fetch ',
+				event.request.url
+			);
+			console.log(response);
+
+			return response;
+		})
+		.catch(function (error) {
+			console.warn('[ServiceWorker|Post] Error from fetch: ', error);
+		});
+	const testCourse = {
+		description: "offline created course",
+		id: "12345678",
+		maxCodingID: "0",
+		name: "My first offline course",
+	};
+	return new Response(JSON.stringify(testCourse));
 };
 
 /*** Register Routes ***/
-
-get("qdacity.project.insertProject").then(method =>
+function registerPostRoutes() {
 	workbox.routing.registerRoute(
-		pathToRegex(method),
+		pathToRegex(apiMethods["qdacity.project.insertProject"]),
 		insertProjectHandler,
 		'POST'
-	)
-);
+	);
+	workbox.routing.registerRoute(
+		pathToRegex(apiMethods["qdacity.course.insertCourse"]),
+		insertCourseHandler,
+		'POST'
+	);
+}
