3057a2673b5a22abee2a8154ba7d287bda5524cc
Andreas Kaufmann <andreas.kaufmann@fau.de>
2018-04-22 15:50:43 +0200

    added separate jest configuration for acceptacne tests

--- war/jest.config.js
+++ war/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   verbose: true,
   collectCoverage: true,
-  coverageDirectory: "<rootDir>/jest-coverage/",
-  testMatch : [ '**/tests/acceptance-tests/**/*test.js' ]
+  coverageDirectory: "<rootDir>/jest-coverage/"
 };
--- war/tests/acceptance-tests/suite.test.js
+++
@@ -1,2 +0,0 @@
-require('./00_LoadQDAcityTest.js');
-require('./01_LoginTest.js');
