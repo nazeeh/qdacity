a3077a89796ad1245b489b78ee55a8fa4ede7757
Georg Schwarz <georg.schwarz@fau.de>
2018-04-30 08:37:22 +0200

    my constants for repo

--- src/com/qdacity/Constants.java
+++ src/com/qdacity/Constants.java
@@ -1,11 +1,11 @@
 package com.qdacity;
 
 public class Constants {
-	public static final String WEB_CLIENT_ID = "309419937441-6d41vclqvedjptnel95i2hs4hu75u4v7.apps.googleusercontent.com";
+	public static final String WEB_CLIENT_ID = "385639345148-k8ph3ug699i8974d8f8vjcffd1tr9llg.apps.googleusercontent.com";
  	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
 	public static final String VERSION = "v15";
 	public static final String BILLING_TABLE = "qdacity-app.qdacity_billing.gcp_billing_export_v1_005083_BC6F93_4D2352";
-
+	
 	public static final String TWITTER_CONSUMER_KEY = "8WjTuE2g5hc5FJA4nsY5ykXCa";
 	public static final String TWITTER_CONSUMER_SECRET = "XOgdbf7LN79OmIsIBAT7jnzqU4AfIRtaFIYEBfQCyfVopoxH0N";
 }
