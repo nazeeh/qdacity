9b0173b1eea5fc51fc559c5c11bd820553e2461e
Sebastian Knauer <sebastian.knauer@fau.de>
2018-05-03 12:56:33 +0200

    update deploy info

--- src/com/qdacity/Constants.java
+++ src/com/qdacity/Constants.java
@@ -1,9 +1,9 @@
 package com.qdacity;
 
 public class Constants {
-	public static final String WEB_CLIENT_ID = "309419937441-6d41vclqvedjptnel95i2hs4hu75u4v7.apps.googleusercontent.com";
+	public static final String WEB_CLIENT_ID = "904920651410-9391vhj0jheq27c3a854k4jaai1f6ssq.apps.googleusercontent.com";
  	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
-	public static final String VERSION = "v15";
+	public static final String VERSION = "v14";
 	public static final String BILLING_TABLE = "qdacity-app.qdacity_billing.gcp_billing_export_v1_005083_BC6F93_4D2352";
 
 	public static final String TWITTER_CONSUMER_KEY = "8WjTuE2g5hc5FJA4nsY5ykXCa";
