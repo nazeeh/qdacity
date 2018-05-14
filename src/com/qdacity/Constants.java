a34f76511bdf55ed09a2cfe77f18c7206dcf2d42
Georg Schwarz <georg.schwarz@fau.de>
2018-05-14 07:46:05 +0200

    adjust settings to my repo

--- src/com/qdacity/Constants.java
+++ src/com/qdacity/Constants.java
@@ -1,8 +1,8 @@
 package com.qdacity;
 
 public class Constants {
-	public static final String WEB_CLIENT_ID = "309419937441-6d41vclqvedjptnel95i2hs4hu75u4v7.apps.googleusercontent.com";
- 	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+	public static final String WEB_CLIENT_ID = "385639345148-k8ph3ug699i8974d8f8vjcffd1tr9llg.apps.googleusercontent.com";
+	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
 	public static final String VERSION = "v15";
 	public static final String BILLING_TABLE = "qdacity-app.qdacity_billing.gcp_billing_export_v1_005083_BC6F93_4D2352";
 
