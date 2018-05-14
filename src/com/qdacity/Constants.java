4a52aefafc9c6e2691acadefb090393cb6773250
Andreas Kaufmann <andreas.kaufmann@fau.de>
2018-05-14 17:45:34 +0200

    Bumped version to v17

--- src/com/qdacity/Constants.java
+++ src/com/qdacity/Constants.java
@@ -3,7 +3,7 @@ package com.qdacity;
 public class Constants {
 	public static final String WEB_CLIENT_ID = "309419937441-6d41vclqvedjptnel95i2hs4hu75u4v7.apps.googleusercontent.com";
  	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
-	public static final String VERSION = "v16";
+	public static final String VERSION = "v17";
 	public static final String BILLING_TABLE = "qdacity-app.qdacity_billing.gcp_billing_export_v1_005083_BC6F93_4D2352";
 
 	public static final String TWITTER_CONSUMER_KEY = "8WjTuE2g5hc5FJA4nsY5ykXCa";
